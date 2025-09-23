e8fc2f605d9b4a215501f19fc179d2d1520deb5e
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-23 09:34:12 +0300

    Documented more the TaskSchedule

--- core/src/schedule.rs
+++ core/src/schedule.rs
@@ -11,30 +11,61 @@ pub use crate::schedule::interval::TaskScheduleInterval;
 
 use chrono::{DateTime, Local};
 use std::error::Error;
+use std::ops::Deref;
 use std::sync::Arc;
+use crate::task::{Task};
 
-/// The [`TaskSchedule`] trait defines when a task should be executed, by default they can be in 3 forms:
+/// The [`TaskSchedule`] trait is used to calculate the next point of time given a time instance
+/// where the task will be scheduled to execute. This system is used closely by the [`Scheduler`]
+/// and the [`Task`]
+///
+/// # Required Methods
+/// If one wants to implement this trait, they must provide an implementation for the
+/// [`TaskSchedule::next_after`] method used to calculate the next available time
+///
+/// # Trait Implementation(s)
+/// some of the noteworthy trait implementation of this trait include:
 /// - [`TaskScheduleInterval`] executes a task on an interval basis
 /// - [`TaskScheduleCalendar`] executes a task based on the provided cron expression as a string
 /// - [`TaskScheduleCron`] defines a human-friendly schedule on when the task runs, it provides fine-grain
 ///   control on each individual field via [`TaskCalendarField`], it can be at an exact date, an interval basis... etc.
 ///   It is a good alternative to cron, as it provides second and millisecond accuracy plus being more human-friendly
 ///
-/// The schedule by itself is only useful when used together with a [`Scheduler`]
+/// This trait is also implemented for any type ``Arc<T>`` where ``T`` is itself
+/// an implementation of the [`TaskSchedule`] trait, making it relatively easy to store both
+/// owned and non-owned values
 ///
-/// # See
+/// # See Also
 /// - [`Scheduler`]
 /// - [`TaskScheduleInterval`]
 /// - [`TaskScheduleCalendar`]
 /// - [`TaskScheduleCron`]
+/// - [`Task`]
 pub trait TaskSchedule: Send + Sync {
-    /// Calculates the future time to execute, this may return an error in the process if unable due
-    /// to any reason, read more on the trait implementation's documentation to learn more
+    /// Calculates the next point in time to schedule the [`Task`] via a specific point in time. This
+    /// method is called automatically by the [`Scheduler`] in either registration or rescheduling
+    ///
+    /// # Arguments
+    /// It accepts a ``time`` reference which is a local time used as a basis for calculating the
+    /// future time to execute at
+    ///
+    /// # Returns
+    /// A ``Result<DateTime<Local>, Arc<dyn Error>>`` which when successful, returns the calculated
+    /// local time, otherwise it returns an error wrapped in an Arc
+    ///
+    /// # See Also
+    /// - [`Scheduler`]
+    /// - [`TaskSchedule`]
+    /// - [`Task`]
     fn next_after(&self, time: &DateTime<Local>) -> Result<DateTime<Local>, Arc<dyn Error>>;
 }
 
-impl<TS: TaskSchedule + ?Sized> TaskSchedule for Arc<TS> {
+impl<T> TaskSchedule for T
+where
+    T: Deref + Send + Sync,
+    T::Target: TaskSchedule
+{
     fn next_after(&self, time: &DateTime<Local>) -> Result<DateTime<Local>, Arc<dyn Error>> {
-        self.as_ref().next_after(time)
+        self.deref().next_after(time)
     }
 }
