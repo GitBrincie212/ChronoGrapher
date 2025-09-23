71e90a775e49a673073f50e293db38f78c2660c8
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-23 09:49:32 +0300

    Documented more heavily TaskPriority

--- core/src/task/priority.rs
+++ core/src/task/priority.rs
@@ -1,7 +1,39 @@
-/// [`TaskPriority`] dictates the importance of a task, the more important a task is,
+#[allow(unused_imports)]
+use crate::task::Task;
+
+#[allow(unused_imports)]
+use crate::scheduler::task_dispatcher::SchedulerTaskDispatcher;
+
+/// [`TaskPriority`] dictates the importance of a [`Task`], the more important a task is,
 /// the more ChronoGrapher ensures to execute the task at a specific time without any latency,
-/// no matter what, the lower level of the spectrum is [`TaskPriority::LOW`] while the highest it
-/// can be is [`TaskPriority::CRITICAL`] where no single time drift is allowed
+/// no matter what. Priorities are mostly used and handled in [`SchedulerTaskDispatcher`]
+///
+/// # Variants
+/// - [`TaskPriority::LOW`] It is the lowest level of priority and represents a non-important background task,
+/// time drifts are bound to happen under heavy workflow
+///
+/// - [`TaskPriority::MODERATE`] It is a semi non-important background task, slight time drifts may
+/// happen under heavier workflow
+///
+/// - [`TaskPriority::HIGH`] It is slightly important task, time drifts are rarer but when they happen
+/// they are at such small barely noticeable scale under very heavy workflow
+///
+/// - [`TaskPriority::IMPORTANT`] It is an important task, time drifts are improbable but still possible,
+/// again they are at such small barely noticeable scale under extreme heavy workflow. ChronoGrapher
+/// ensures that this type of priority is executed at almost the exact time
+///
+/// - [`TaskPriority::CRITICAL`] It is the highest level of priority, and represents a critical task
+/// that has to be executed at this time specifically. No time drifts are allowed, exact laser focus
+/// execution
+///
+/// # Trait Implementation(s)
+/// The enum is cloneable and copiable via [`Clone`] and [`Copy`] traits respectively, it also
+/// implements the [`Debug`] trait, as well as equality and ord via [`PartialEq`], [`PartialOrd`],
+/// [`Ord`] and [`Eq`]. In addition to the [`Default`] trait with default value [`TaskPriority::MODERATE`]
+///
+/// # See Also
+/// - [`Task`]
+/// - [`SchedulerTaskDispatcher`]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TaskPriority {
