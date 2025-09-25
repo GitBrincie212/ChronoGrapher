7ca90712a3a437c011dab62d29316ac0a2b6f59b
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-25 13:08:04 +0300

    Documented more heavily the TaskContext and TskFrame

--- core/src/task/frames.rs
+++ core/src/task/frames.rs
@@ -31,20 +31,75 @@ use std::sync::atomic::Ordering;
 use std::time::Duration;
 pub use timeoutframe::TimeoutTaskFrame;
 
+/// A task-related error (i.e. A task failure)
 pub type TaskError = Arc<dyn Debug + Send + Sync>;
 
+/// [`TaskContext`] is a mechanism that wraps commonly needed information
+/// inside it which can be accessed by [`TaskFrame`], it essentially wraps
+/// - metadata: [`TaskMetadata`]
+/// - emitter: [`TaskEventEmitter`]
+/// - priority: [`TaskPriority`]
+/// - runs: ``u64``
+/// - debug_label: ``String``
+/// - max_runs: ``Option<NonZeroU64>``
+///
+/// All of them fetched in [`Task`]
+///
+/// # Constructor(s)
+/// There are no public constructors as this context's constructor is sealed
+///
+/// # Task Implementation(s)
+/// The [`TaskContext`] only implements [`Clone`] and [`Debug`] as there is no other use for it.
+/// Where for [`Debug`] it outputs all the fields except the event emitter (it holds no data, so no
+/// point in recording that)
+///
+/// # See Also
+/// - [`Task`]
+/// - [`TaskFrame`]
+/// - [`TaskMetadata`]
+/// - [`TaskPriority`]
+/// - [`TaskEventEmitter`]
 #[derive(Clone)]
 pub struct TaskContext {
-    pub metadata: Arc<TaskMetadata>,
-    pub emitter: Arc<TaskEventEmitter>,
-    pub priority: TaskPriority,
-    pub runs: u64,
-    pub debug_label: String,
-    pub max_runs: Option<NonZeroU64>,
+    pub(crate) metadata: Arc<TaskMetadata>,
+    pub(crate) emitter: Arc<TaskEventEmitter>,
+    pub(crate) priority: TaskPriority,
+    pub(crate) runs: u64,
+    pub(crate) debug_label: String,
+    pub(crate) max_runs: Option<NonZeroU64>,
+}
+
+impl Debug for TaskContext {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("TaskContext")
+            .field("metadata", self.metadata.as_ref())
+            .field("priority", &self.priority)
+            .field("runs", &self.runs)
+            .field("debug_label", &&self.debug_label)
+            .field("max_runs", &self.max_runs)
+            .finish()
+    }
 }
 
 impl TaskContext {
-    pub fn new(task: &Task, emitter: Arc<TaskEventEmitter>) -> Arc<Self> {
+    /// Constructs / Creates a new [`TaskContext`] instance based for use inside [`TaskFrame`],
+    /// unlike most constructors, this mechanism is sealed and accessible only in the library's
+    /// internal code
+    ///
+    /// # Argument(s)
+    /// This method accepts 2 arguments, those being [`Task`] and [`TaskEventEmitter`] wrapped in an
+    /// ``Arc<T>``, the former is for retrieving most of the data. While the latter is not only to prove
+    /// that this construction is made internally on [`Task`] but also to share it with [`TaskFrame`]
+    /// to emit its own events
+    ///
+    /// # Returns
+    /// The constructed instance to be used
+    /// 
+    /// # See Also
+    /// - [`Task`]
+    /// - [`TaskEventEmitter`]
+    /// - [`TaskContext`]
+    pub(crate) fn new(task: &Task, emitter: Arc<TaskEventEmitter>) -> Arc<Self> {
         Arc::new(Self {
             metadata: task.metadata.clone(),
             emitter,
@@ -54,12 +109,87 @@ impl TaskContext {
             max_runs: task.max_runs,
         })
     }
+
+    /// Accesses the metadata field, returning it in the process
+    /// 
+    /// # Returns
+    /// The metadata field as an ``Arc<TaskMetadata>``
+    /// 
+    /// # See Also
+    /// - [`TaskContext`]
+    /// - [`TaskMetadata`]
+    pub fn metadata(&self) -> Arc<TaskMetadata> {
+        self.metadata.clone()
+    }
+
+    /// Accesses the event emitter field, returning it in the process
+    ///
+    /// # Returns
+    /// The event emitter field as an ``Arc<TaskEventEmitter>``
+    ///
+    /// # See Also
+    /// - [`TaskContext`]
+    /// - [`TaskEventEmitter`]
+    pub fn emitter(&self) -> Arc<TaskEventEmitter> {
+        self.emitter.clone()
+    }
+
+    /// Accesses the priority field, returning it in the process
+    ///
+    /// # Returns
+    /// The priority field as an [`TaskPriority`]
+    ///
+    /// # See Also
+    /// - [`TaskContext`]
+    /// - [`TaskPriority`]
+    pub fn priority(&self) -> TaskPriority {
+        self.priority.clone()
+    }
+
+    /// Accesses the runs field (counts how many times the task ran), returning it in the process
+    ///
+    /// # Returns
+    /// The runs field as a typical ``u64``
+    ///
+    /// # See Also
+    /// - [`TaskContext`]
+    pub fn runs(&self) -> u64 {
+        self.runs
+    }
+
+    /// Accesses the debug label field, returning it in the process
+    ///
+    /// # Returns
+    /// The debug label field as a typical ``&str``
+    ///
+    /// # See Also
+    /// - [`TaskContext`]
+    pub fn debug_label(&self) -> &str {
+        self.debug_label.as_str()
+    }
+
+    /// Accesses the max_runs field, returning it in the process
+    ///
+    /// # Returns
+    /// The max_runs field as a ``Option<NonZeroU64>``
+    ///
+    /// # See Also
+    /// - [`TaskContext`]
+    pub fn max_runs(&self) -> Option<NonZeroU64> {
+        self.max_runs.clone()
+    }
 }
 
-/// [`TaskFrame`] represents a unit of work which hosts the actual computation logic for the [`Scheduler`]
-/// to invoke, this is a part of the task system. [`TaskFrame`] encapsulates mainly the async execution logic
-/// of the task which is evaluated by calling [`TaskFrame::execute`] Returning `Ok(())` on success,
-/// or an `Arc<dyn Error>` on failure, as well as events
+/// [`TaskFrame`] represents a unit of work which hosts the actual computation logic for
+/// the [`Scheduler`] to invoke, this is a part of the task system
+///
+/// # Required Method(s)
+/// When one implements the [`TaskFrame`] trait, one has to implement [`TaskFrame::execute`] which
+/// encapsulates mainly the async execution logic of the [`Task`], the method has as argument
+/// a [`TaskContext`], wrapping any essential logic (while also being impossible to create it
+/// outside the [`Task`]), the method also returns either `Ok(())` on success, or a `TaskError`
+/// on failure. The method also handles the emission of local task frame events
+/// (learn more about in [`TaskEvent`])
 ///
 /// # Usage Notes
 /// This is one of many components which are combined to form a task, other components are needed
@@ -90,9 +220,59 @@ impl TaskContext {
 /// task frame `T2` (as a fallback), try/retry executing this task frame for `N` times (can be
 /// controlled by the developer) with a delay per retry of `d` (can be controlled by the developer),
 /// regardless if it succeeds at some time or fails entirely, return the result back
+///
+/// # Object Safety
+/// This trait is object safe to use, as seen in the source code of [`Task`] struct
+///
+/// # Trait Implementation(s)
+/// There are various implementations for [`TaskFrame`] present in the library, each
+/// doing their own part. Some noteworthy mentions include
+/// - [`RetriableTaskFrame`] Retries a task frame a specified number of times with a delay
+/// per retry based on a supplied [`RetryBackoffStrategy`] (more info on the docs of it)
+///
+/// - [`TimeoutTaskFrame`] Runs a task frame for a specified duration, if the countdown reaches
+/// zero, then it halts the task and returns a timeout error (more info on the docs of it)
+///
+/// - [`DependencyTaskFrame`] Before running a task frame, it checks if its dependencies are resolved,
+/// if they are then it runs, otherwise it errors out with dependencies unresolved
+///
+/// - [`FallbackTaskFrame`] Attempts to run a task frame, if it fails, then a fallback secondary task frame
+/// takes its place which the result of this fallback task frame is returned from the secondary (more
+/// info on the docs of it)
+///
+/// - [`NoOperationTaskFrame`] Effectively acts as a placeholder and does nothing useful, in
+/// some circumstances where task frames may be optional to supply (more info on the docs of it)
+///
+/// It is advised to check the submodules of the task module to see more of them in action
+///
+/// # See Also
+/// - [`TaskFrame::execute`]
+/// - [`RetriableTaskFrame`]
+/// - [`TimeoutTaskFrame`]
+/// - [`DependencyTaskFrame`]
+/// - [`FallbackTaskFrame`]
+/// - [`NoOperationTaskFrame`]
+/// - [`ConditionalTaskFrame`]
+/// - [`SelectTaskFrame`]
+/// - [`Task`]
 #[async_trait]
 pub trait TaskFrame: Send + Sync {
-    /// The main execution logic of the task, it is meant as an internal method
+    /// The execution logic of the [`TaskFrame`] and subsequentially [`Task`]
+    ///
+    /// # Argument(s)
+    /// This method accepts one argument, that being ``ctx`` which is the context object,
+    /// this context object is private and cannot be created by outside parties, but only in
+    /// [`Task`]. The context wraps common information for the [`TaskFrame`] to access and pass
+    /// on to other child [`TaskFrame`]s
+    ///
+    /// # Returns
+    /// A ``Result<(), TaskError>`` which on success returns ``Ok(())`` (i.e. No result) and on
+    /// failure it returns a ``Err(TaskError)`` indicating what went wrong on the [`TaskFrame`]
+    ///
+    /// # See Also
+    /// - [`Task`]
+    /// - [`TaskContext`]
+    /// - [`TaskFrame`]
     async fn execute(&self, ctx: Arc<TaskContext>) -> Result<(), TaskError>;
 }
 
