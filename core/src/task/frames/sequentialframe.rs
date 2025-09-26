47ed533e79d4722d3cade2287281ee42ef3d9b79
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-26 16:20:29 +0300

    Reworked documentation for SequentialTaskFrame

--- core/src/task/frames/sequentialframe.rs
+++ core/src/task/frames/sequentialframe.rs
@@ -1,22 +1,34 @@
 use crate::task::{
-    ArcTaskEvent, GroupedTaskFramesExecBehavior, GroupedTaskFramesQuitOnFailure, TaskContext,
-    TaskError, TaskEvent, TaskFrame,
+    ArcTaskEvent, GroupedTaskFramesExecBehavior, GroupedTaskFramesQuitOnFailure,
+    TaskContext, TaskError, TaskEvent, TaskFrame
 };
 use async_trait::async_trait;
 use std::sync::Arc;
 
-/// Represents a **sequential task frame** which wraps multiple task frames to execute at the same time
-/// in a sequential manner. This task frame type acts as a **composite node** within the task frame hierarchy,
-/// facilitating a way to represent multiple task frames which have same timings but depend on each
+#[allow(unused_imports)]
+use crate::task::ParallelTaskFrame;
+
+/// Represents a **sequential task frame** which wraps multiple [`TaskFrame`] to execute at the same time
+/// in a sequential manner. This task frame type acts as a **composite node** within the [`TaskFrame`] hierarchy,
+/// facilitating a way to represent multiple [`TaskFrame`] which have same timings but depend on each
 /// previous task frame finishing. The order of execution is ordered, and thus why its sequential,
-/// in the case where execution order does not matter and tasks do not require sequential execution,
-/// it is advised to use [`ParallelTaskFrame`] as opposed to [`SequentialTaskFrame`]
+/// in the case where execution order matters, it is advised to use [`ParallelTaskFrame`]
+/// as opposed to [`SequentialTaskFrame`]
+///
+/// # Constructor(s)
+/// When constructing a [`SequentialTaskFrame`], one can use either [`SequentialTaskFrame::new`] for no explicit
+/// [`GroupedTaskFramesExecBehavior`] policy (convenience) or [`SequentialTaskFrame::new_with`]
+/// if they do want to specify the [`GroupedTaskFramesExecBehavior`] policy as well
 ///
 /// # Events
-/// For events, [`SequentialTaskFrame`] has 2 of them, these being `on_child_start` and `on_child_end`,
-/// the former is for when a child task frame is about to start, the event hands out the target task
-/// frame. For the latter, it is for when a child task frame ends, the event hands out the target
-/// task frame and an optional error in case it fails
+/// For events, [`SequentialTaskFrame`] has 2 of them, these being [`SequentialTaskFrame::on_child_start`] and
+/// [`SequentialTaskFrame::on_child_end`],the former is for when a child task frame is about to start, the
+/// event hands out the target [`TaskFrame`]. For the latter, it is for when a child task frame ends,
+/// the event hands out the target task frame and an optional error in case it fails
+///
+/// # Trait Implementation(s)
+/// It is obvious that the [`SequentialTaskFrame`] implements [`TaskFrame`] since this
+/// is a part of the default provided implementations, however there are many others
 ///
 /// # Example
 /// ```ignore
@@ -27,21 +39,21 @@ use std::sync::Arc;
 /// use chronographer_core::task::sequentialframe::SequentialTaskFrame;
 ///
 /// let primary_frame = ExecutionTaskFrame::new(
-///     |_metadata| async {
+///     |_ctx| async {
 ///         println!("Primary task frame fired...");
 ///         Ok(())
 ///     }
 /// );
 ///
 /// let secondary_frame = ExecutionTaskFrame::new(
-///     |_metadata| async {
+///     |_ctx| async {
 ///         println!("Secondary task frame fired...");
 ///         Ok(())
 ///     }
 /// );
 ///
 /// let tertiary_frame = ExecutionTaskFrame::new(
-///     |_metadata| async {
+///     |_ctx| async {
 ///         println!("Tertiary task frame fired...");
 ///         Err(())
 ///     }
@@ -59,19 +71,64 @@ use std::sync::Arc;
 ///
 /// CHRONOGRAPHER_SCHEDULER.schedule_owned(task).await;
 /// ```
+/// # See Also
+/// - [`TaskFrame`]
+/// - [`ParallelTaskFrame`]
+/// - [`GroupedTaskFramesExecBehavior`]
 //noinspection DuplicatedCode
 pub struct SequentialTaskFrame {
     tasks: Vec<Arc<dyn TaskFrame>>,
     policy: Arc<dyn GroupedTaskFramesExecBehavior>,
+
+    /// Event fired for when a child [`TaskFrame`] starts execution
     pub on_child_start: ArcTaskEvent<Arc<dyn TaskFrame>>,
+
+    /// Event fired for when a child [`TaskFrame`] has ended execution
     pub on_child_end: ArcTaskEvent<(Arc<dyn TaskFrame>, Option<TaskError>)>,
 }
 
 impl SequentialTaskFrame {
+    /// Creates / Constructs a new [`SequentialTaskFrame`] instance based on
+    /// the child [`TaskFrame`] collection supplied. If one wishes to
+    /// also supply their own [`GroupedTaskFramesExecBehavior`], then they can use
+    /// [`SequentialTaskFrame::new_with`]
+    ///
+    /// # Argument(s)
+    /// This method accepts one single argument, that is the collection of [`TaskFrame`] to wrap
+    /// around the [`SequentialTaskFrame`] to execute concurrently
+    ///
+    /// # Returns
+    /// A fully created [`SequentialTaskFrame`] with the wrapped ``tasks``
+    ///
+    /// # See Also
+    /// - [`TaskFrame`]
+    /// - [`GroupedTaskFramesExecBehavior`]
+    /// - [`SequentialTaskFrame::new_with`]
+    /// - [`SequentialTaskFrame`]
     pub fn new(tasks: Vec<Arc<dyn TaskFrame>>) -> SequentialTaskFrame {
         Self::new_with(tasks, GroupedTaskFramesQuitOnFailure)
     }
 
+    /// Creates / Constructs a new [`SequentialTaskFrame`] instance based on
+    /// the child [`TaskFrame`] collection and a [`GroupedTaskFramesExecBehavior`] policy supplied.
+    /// If one wishes to prefer the default [`GroupedTaskFramesExecBehavior`], then they can use
+    /// [`SequentialTaskFrame::new`] for convenience
+    ///
+    /// # Argument(s)
+    /// This method accepts two arguments, those being the collection of [`TaskFrame`] to wrap
+    /// around the [`SequentialTaskFrame`] to execute concurrently and a [`GroupedTaskFramesExecBehavior`]
+    /// policy
+    ///
+    /// # Returns
+    /// A fully created [`SequentialTaskFrame`] with the wrapped ``tasks`` and a custom ``policy``
+    /// as a [`GroupedTaskFramesExecBehavior`]
+    /// ``
+    ///
+    /// # See Also
+    /// - [`TaskFrame`]
+    /// - [`GroupedTaskFramesExecBehavior`]
+    /// - [`SequentialTaskFrame::new`]
+    /// - [`SequentialTaskFrame`]
     pub fn new_with(
         tasks: Vec<Arc<dyn TaskFrame>>,
         sequential_policy: impl GroupedTaskFramesExecBehavior + 'static,
