69e64611dd68e0c5e176cbcc331b1105dcb2fbdd
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-26 12:27:02 +0300

    Removed the policy_match macro in favour of GroupedTaskFramesExecBehavior

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,59 +1,6 @@
 use chrono::{DateTime, Local, TimeZone};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-#[macro_export]
-macro_rules! policy_match {
-    (
-        $metadata: expr, $emitter: expr, $task: expr,
-        $self: expr, $result: expr, $policy_enum: ident
-    ) => {{
-        match ($result, &$self.policy) {
-            (Err(error), &$policy_enum::RunUntilFailure) => {
-                $emitter
-                    .clone()
-                    .emit(
-                        $metadata.clone(),
-                        $self.on_child_end.clone(),
-                        ($task.clone(), Some(error.clone())),
-                    )
-                    .await;
-                return Err(error);
-            }
-            (Ok(res), &$policy_enum::RunUntilSuccess) => {
-                $emitter
-                    .clone()
-                    .emit(
-                        $metadata.clone(),
-                        $self.on_child_end.clone(),
-                        ($task.clone(), None),
-                    )
-                    .await;
-                return Ok(res);
-            }
-            (Err(error), _) => {
-                $emitter
-                    .clone()
-                    .emit(
-                        $metadata.clone(),
-                        $self.on_child_end.clone(),
-                        ($task.clone(), Some(error.clone())),
-                    )
-                    .await;
-            }
-            (Ok(_), _) => {
-                $emitter
-                    .clone()
-                    .emit(
-                        $metadata.clone(),
-                        $self.on_child_end.clone(),
-                        ($task.clone(), None),
-                    )
-                    .await;
-            }
-        }
-    }};
-}
-
 pub fn system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
     let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
         Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
