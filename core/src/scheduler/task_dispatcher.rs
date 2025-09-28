09ef4e3178a1cb66a179d89250395d05b4fc458c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-28 18:05:14 +0300

    Expanded the documentation for SchedulerTaskDispatcher and chaanged the sender argument to match recent changes made to the scheduler

--- core/src/scheduler/task_dispatcher.rs
+++ core/src/scheduler/task_dispatcher.rs
@@ -9,15 +9,48 @@ use async_trait::async_trait;
 use std::sync::Arc;
 use tokio::sync::broadcast::Sender;
 
+#[allow(unused_imports)]
+use crate::scheduler::Scheduler;
+
 /// [`SchedulerTaskDispatcher`] is a trait for implementing a scheduler task dispatcher. It acts as
-/// a central point for when a task wants to execute, on the default implementation, it routes the
-/// task to a thread pool based on its priority. Allowing ChronoGrapher to stay responsive even when
-/// under heavy task workload
+/// a central point for when a task wants to execute, it does not handle scheduling, in fact it
+/// communicates closely to the [`Scheduler`] to stay informed on whenever to execute the task
+///
+/// # Required Method(s)
+/// When implementing the [`SchedulerTaskDispatcher`], one has to supply an implementation for
+/// the method [`SchedulerTaskDispatcher::dispatch`] which is where the dispatching logic lives
+///
+/// # Trait Implementation(s)
+/// There is one implementation in particular for [`SchedulerTaskDispatcher`], that is [`DefaultTaskDispatcher`]
+/// which has a thread pool that handles work-stealing and priority management to ensure that
+/// ChronoGrapher stays responsive even under heavy workflow
+///
+/// # Object Safety
+/// [`SchedulerTaskDispatcher`] is object safe as seen throughout [`Scheduler`] source code
+///
+/// # See Also
+/// - [`SchedulerTaskDispatcher::dispatch`]
+/// - [`Scheduler`]
+/// - [`DefaultTaskDispatcher`]
 #[async_trait]
 pub trait SchedulerTaskDispatcher: Debug + Send + Sync {
+    /// The main logic of the [`SchedulerTaskDispatcher`]. This is where it handles
+    /// how to execute a specified task and notify the [`Scheduler`] accordingly
+    ///
+    /// # Argument(s)
+    /// The method accepts 4 arguments, those being a ``sender`` for notifying the
+    /// [`Scheduler`] when the [`Task`] has finished executing, an ``emitter`` which is
+    /// for event emission (the dispatcher by itself doesn't emit events. It is used
+    /// as an argument for running a task), a [`Task`] represented as ``task`` and an
+    /// index pointing to the [`Task`]
+    ///
+    /// # See Also
+    /// - [`Task`]
+    /// - [`Scheduler`]
+    /// - [`SchedulerTaskDispatcher`]
     async fn dispatch(
         self: Arc<Self>,
-        sender: Arc<Sender<(Arc<Task>, usize)>>,
+        sender: Arc<Sender<usize>>,
         emitter: Arc<TaskEventEmitter>,
         task: Arc<Task>,
         idx: usize,
@@ -28,7 +61,7 @@ pub trait SchedulerTaskDispatcher: Debug + Send + Sync {
 impl<TD: SchedulerTaskDispatcher + ?Sized> SchedulerTaskDispatcher for Arc<TD> {
     async fn dispatch(
         self: Arc<Self>,
-        sender: Arc<Sender<(Arc<Task>, usize)>>,
+        sender: Arc<Sender<usize>>,
         emitter: Arc<TaskEventEmitter>,
         task: Arc<Task>,
         idx: usize,
