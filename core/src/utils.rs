15ad3ff6c78e072e942055bafaf0af11687ab0da
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-10-02 14:20:50 +0300

    Added a couple of macro utilities for serialization and deserialization

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,6 +1,59 @@
 use chrono::{DateTime, Local, TimeZone};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+#[macro_export]
+macro_rules! to_json {
+    ($val: expr) => {{
+        serde_json::to_value(
+            $val
+        ).map_err(|x| Arc::new(x) as Arc<dyn Debug + Send + Sync>)?
+    }};
+}
+
+#[macro_export]
+macro_rules! deserialize_field {
+    ($repr: expr, $var_name: ident, $target: expr, $target_type: ty, $error_msg: expr) => {
+        if $repr.contains_key($target) {
+            return Err(deserialization_err!(
+                $repr,
+                $target_type,
+                $error_msg
+            ))
+        }
+        
+        let $var_name = $repr.remove($target).unwrap();
+    };
+}
+
+#[macro_export]
+macro_rules! deserialization_err {
+    ($repr: expr, $target_type: ty, $error_msg: expr) => {{
+        Arc::new(ChronographerErrors::DeserializationFailed(
+            stringify!($target_type).to_string(),
+            $error_msg.to_string(),
+            $repr.clone()
+        )) as Arc<dyn Debug + Send + Sync>
+    }};
+}
+
+#[macro_export]
+macro_rules! acquire_mut_ir_map {
+    ($target_type: ty, $component: expr) => {{
+        let repr = $component.into_ir();
+
+        match repr {
+            serde_json::Value::Object(map) => map,
+            other => {
+                return Err(Arc::new(ChronographerErrors::NonObjectDeserialization(
+                    stringify!($target_type).to_string(),
+                    other
+                )) as Arc<dyn Debug + Send + Sync>)
+            }
+        }
+    }};
+}
+
+
 /// Simply converts the ``SystemTime`` to a ``DateTime<Local>``, it is a private
 /// method used internally by ChronoGrapher, as such why it lives in utils module
 pub fn system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
