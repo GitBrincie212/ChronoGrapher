e3dd416f21e53c3021e4945de0554152f93a9f48
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-10-25 13:09:23 +0300

    Added a macro for generating basic events more easily

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,15 +1,16 @@
 use crate::errors::ChronographerErrors;
 use crate::persistent_object::{AsPersistent, PersistenceCapability, PersistentObject};
 use crate::serialized_component::SerializedComponent;
-use crate::task::TaskError;
+use crate::task::{TaskError, TaskHook, TaskHookContainer, TaskHookEvent};
 use chrono::{DateTime, Local, TimeZone};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use serde_json::Map;
-use std::any::{type_name, type_name_of_val};
+use std::any::{type_name, type_name_of_val, TypeId};
 use std::fmt::Debug;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::task::JoinSet;
 
 pub struct PersistenceUtils(());
 
@@ -120,6 +121,55 @@ impl PersistenceUtils {
     }
 }
 
+#[macro_export]
+macro_rules! define_event {
+    ($(#[$($attrss:tt)*])* $name: ident, $payload: ty) => {
+        #[doc =
+            concat!(
+                "[`", stringify!($name), "`] is an implementation of [`TaskHookEvent`] (a system used \
+                closely with [`TaskHook`]). The concrete payload type of [`", stringify!($name),
+                "`] is ``", stringify!($payload), "``"
+            )
+        ]
+        ///
+        /// # Constructor(s)
+        #[doc =
+            concat!(
+                "When constructing a [`", stringify!($name), "`] due to the fact this is a marker ``struct``, \
+                making it as such zero-sized, one can either use [`", stringify!($name), "::default`]
+                or via simply pasting the struct name ([`", stringify!($name), "`])"
+            )
+        ]
+        ///
+        /// # Trait Implementation(s)
+        #[doc =
+            concat!(
+                "It is obvious that [`", stringify!($name), "`] implements the [`TaskHookEvent`], but also many \
+                other traits such as [`Default`], [`Clone`], [`Copy`], [`Debug`], [`PartialEq`], [`Eq`] \
+                and [`Hash`] from the standard Rust side, as well as [`Serialize`] and [`Deserialize`]"
+            )
+        ]
+        ///
+        /// # Cloning Semantics
+        #[doc = concat!(
+            "When cloning / copy a [`", stringify!($name), "`] it fully creates a \
+            new independent version of that instance"
+        )]
+        ///
+        $(#[$($attrss)*])*
+        /// - [`TaskHook`]
+        /// - [`TaskHookEvent`]
+        /// - [`Task`]
+        /// - [`TaskFrame`]
+        #[derive(Default, Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+        pub struct $name;
+        impl<'a> TaskHookEvent for $name {
+            type Payload = $payload;
+            const PERSISTENCE_ID: &'static str = concat!("chronographer_core#", stringify!($name));
+        }
+    };
+}
+
 /// Simply converts the ``SystemTime`` to a ``DateTime<Local>``, it is a private
 /// method used internally by ChronoGrapher, as such why it lives in utils module
 pub(crate) fn system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
@@ -147,4 +197,18 @@ pub(crate) fn date_time_to_system_time(dt: DateTime<impl TimeZone>) -> SystemTim
     } else {
         UNIX_EPOCH - Duration::from_nanos((-duration_since_epoch) as u64)
     }
+}
+
+pub(crate) async fn emit_event<E: TaskHookEvent>(hooks_container: &TaskHookContainer, payload: &E::Payload) {
+    let hooks = hooks_container.0.get(&TypeId::of::<E>())
+        .map(|x| x.value())
+        .unwrap_or_default();
+
+    let mut set = JoinSet::new();
+
+    for hook in hooks {
+        set.spawn(hook.on_emit(&payload));
+    }
+
+    set.join_all().await;
 }
