6f3d26c23ed3789d5b909f4fc69873a3fcc657cf
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-10-26 09:30:09 +0200

    Slight fix

--- core/src/task/frames/parallelframe.rs
+++ core/src/task/frames/parallelframe.rs
@@ -150,7 +150,7 @@ impl TaskFrame for ParallelTaskFrame {
                         let result_tx = result_tx.clone();
                         s.spawn(move || {
                             tokio::spawn(async move {
-                                context_clone.emit::<OnChildEnd>(&frame_clone).await;
+                                context_clone.emit::<OnChildStart>(&frame_clone).await;
                                 let result = frame_clone.execute(context_clone.clone()).await;
                                 context_clone
                                     .emit::<OnChildEnd>(&(frame_clone, result.clone().err()))
