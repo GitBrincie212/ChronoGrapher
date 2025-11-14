4cc352016292c3b175c649dc95f6d3f8c3d34775
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-11-14 13:26:13 +0200

    A couple of small changes

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,15 +1,15 @@
-use chronographer_core::scheduler::CHRONOGRAPHER_SCHEDULER;
-use chronographer_core::task::{ExecutionTaskFrame, Task, TaskScheduleInterval};
+use chronographer::scheduler::CHRONOGRAPHER_SCHEDULER;
+use chronographer::task::{Task, TaskScheduleInterval};
 
 #[tokio::main]
 #[allow(clippy::empty_loop)]
 async fn main() {
     let task = Task::define(
         TaskScheduleInterval::from_secs(4),
-        ExecutionTaskFrame::new(|_ctx| async {
+        |_ctx| async {
             println!("Hello World");
             Ok(())
-        }),
+        }
     );
 
     CHRONOGRAPHER_SCHEDULER.schedule_owned(task).await;
