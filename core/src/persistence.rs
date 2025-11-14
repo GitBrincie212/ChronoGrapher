4365217ea6fcb3e66f8ad3f723a73a461b796353
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-11-14 13:22:21 +0200

    Shrank the size of persistence module and made it use serde more directly. Will add more functionality later as needed

--- core/src/persistence.rs
+++ core/src/persistence.rs
@@ -1,78 +1,28 @@
+use serde::{Deserialize, Serialize};
+
 #[allow(unused_imports)]
 use crate::backend::PersistenceBackend;
-use crate::serialized_component::SerializedComponent;
-use crate::task::TaskError;
-#[allow(unused_imports)]
-use crate::task::*;
-use async_trait::async_trait;
-use std::any::type_name;
-use std::ops::{Deref, DerefMut};
-use std::sync::OnceLock;
 
-/// [`PersistenceCapability`] is a small utility for controlling the purpose of an object,
+/// [`PersistenceObject`] is a trait used for serialization and deserialization (via serde) while
+/// also having an associated Persistence ID (Identifier), which is used for tracking the concrete type via
+/// a string that is guarantee to be unique. For creating a unique identifier, one recommended format to
+/// use is:
+/// ```ignore
+/// [CRATE]::[TYPE_NAME]#[CUSTOM UUID]
+/// ```
+/// For UUID generation, we recommend using https://www.uuidgenerator.net/version4.
+/// The system is used closely with [`PersistenceBackend`]
 ///
-pub enum PersistenceCapability<'a> {
-    NotPersistable,
-    Ephemeral,
-    Persistable(&'a dyn PersistentObjectDyn),
-}
-
-/// [`PersistentObject`] is a trait for implementing a persistent object, this trait
-/// is used throughout most task composites, such as [`TaskFrame`], [`TaskEvent`],
-/// [`TaskMetadata`]... etc. This system is made to be backend agnostic
+/// # Supertrait(s)
+/// When implementing [`PersistenceObject`], one has to also implement [`Serialize`] and
+/// [`Deserialize`] traits, as they are the backbone to what allows serialization and deserialization
+/// respectively
 ///
-/// # Required Method(s)
-/// When implementing the [`PersistentObject`], one must implement 2 methods, those
-/// being [`PersistentObject::store`] and [`PersistentObject::retrieve`], the former
-/// is used to serialize into a [`SerializedComponent`] which is an intermediate representation
-/// of the type, for the [`PersistenceBackend`] to handle accordingly the full serialization. While
-/// the latter is used for deserialization where it accepts the intermediate representation (the
-/// way it knows this representation corresponds to the type is via the [`SerializedComponent::id`]
-/// and the deserializer register system).
-///
-/// # Trait Implementation(s)
-/// Any type that implements [`Serialize`] and [`Deserialize`] from serde, automatically implements
-/// this trait (as such, this integration is handy). In addition, most provided implementations of
-/// various systems by ChronoGrapher implement the [`PersistentObject`] trait
+/// # Object Safety
+/// [`PersistenceObject`] is not object safe as it contains an associated constant
 ///
 /// # See Also
-/// - [`TaskFrame`]
-/// - [`TaskMetadata`]
-/// - [`TaskEvent`]
-/// - [`SerializedComponent`]
 /// - [`PersistenceBackend`]
-#[async_trait]
-pub trait PersistentObject: Send + Sync {
-    fn persistence_id() -> &'static str {
-        static CELL: OnceLock<String> = OnceLock::new();
-        CELL.get_or_init(|| type_name::<Self>().to_string())
-            .as_str()
-    }
-
-    async fn persist(&self) -> Result<SerializedComponent, TaskError>;
-    async fn retrieve(component: SerializedComponent) -> Result<Self, TaskError>
-    where
-        Self: Sized;
+pub trait PersistenceObject: Serialize + for<'de> Deserialize<'de> + Send + Sync {
+    const PERSISTENCE_ID: &'static str;
 }
-
-#[async_trait]
-pub trait PersistentObjectDyn: Send + Sync {
-    async fn persist(&self) -> Result<SerializedComponent, TaskError>;
-}
-
-#[async_trait]
-impl<T: PersistentObject> PersistentObjectDyn for T {
-    async fn persist(&self) -> Result<SerializedComponent, TaskError> {
-        self.persist().await
-    }
-}
-
-#[async_trait]
-pub trait AsPersistent {
-    async fn as_persistent(&self) -> PersistenceCapability {
-        PersistenceCapability::NotPersistable
-    }
-}
-
-#[async_trait]
-impl<T: ?Sized> AsPersistent for T {}
