4f899540c91e3053f58f4e5148b3ea5650e528d4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-12-11 15:51:11 +0200

    Updated the main for testing the throughput of the scheduler

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,14 +1,35 @@
-use chronographer::scheduler::CHRONOGRAPHER_SCHEDULER;
-use chronographer::task::{Task, TaskScheduleInterval};
+use std::sync::LazyLock;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+use tokio::task::yield_now;
+use chronographer::dynamic_taskframe;
+use chronographer::prelude::*;
+
+static COUNTER: LazyLock<AtomicUsize> = LazyLock::new(|| AtomicUsize::new(0));
 
 #[tokio::main]
 #[allow(clippy::empty_loop)]
 async fn main() {
-    let task = Task::define(TaskScheduleInterval::from_secs(4), |_ctx| async {
-        println!("Hello World");
-        Ok(())
-    });
+    dbg!("LOADING TASKS");
+    let mut millis: f64 = 0.9;
+    for _ in 0..200_000 {
+        millis *= 0.05;
+        let task = Task::simple(
+            TaskScheduleInterval::from_secs_f64(millis),
+            dynamic_taskframe!({
+                // println!("{}", INSTANT.elapsed().as_secs_f64());
+                yield_now().await;
+                COUNTER.fetch_add(1, Ordering::Relaxed);
+                Ok(())
+            })
+        );
 
-    CHRONOGRAPHER_SCHEDULER.schedule_owned(task).await;
-    loop {}
+        CHRONOGRAPHER_SCHEDULER.schedule_owned(task).await;
+    }
+    dbg!("STARTING");
+
+    CHRONOGRAPHER_SCHEDULER.start().await;
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    CHRONOGRAPHER_SCHEDULER.abort().await;
+    println!("{}", COUNTER.load(Ordering::Relaxed));
 }
