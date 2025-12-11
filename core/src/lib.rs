3ae94ba5d00e15d801579ca30c590f0b5fa35088
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-12-11 15:51:31 +0200

    Added a prelude module, in addition to moving some of the other modules to the correct places

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,19 +1,51 @@
-pub mod clock; // skipcq: RS-D1001
-
 pub mod errors; // skipcq: RS-D1001
 
-pub mod schedule; // skipcq: RS-D1001
-
 pub mod scheduler; // skipcq: RS-D1001
-
-pub mod scheduling_strats; // skipcq: RS-D1001
 
 pub mod task; // skipcq: RS-D1001
 
 pub mod utils; // skipcq: RS-D1001
 
-pub mod backend; // skipcq: RS-D1001
-
 pub mod persistence; // skipcq: RS-D1001
 
-pub mod retrieve_registers; // skipcq: RS-D1001
+pub mod prelude {
+    // Core
+    pub use crate::task::{Task, TaskContext, TaskError};
+    pub use crate::scheduler::CHRONOGRAPHER_SCHEDULER;
+
+    // Common frames
+    pub use crate::task::delayframe::DelayTaskFrame;
+    pub use crate::task::timeoutframe::TimeoutTaskFrame;
+    pub use crate::task::retryframe::RetriableTaskFrame;
+    pub use crate::task::fallbackframe::FallbackTaskFrame;
+    pub use crate::task::sequentialframe::SequentialTaskFrame;
+    pub use crate::task::parallelframe::ParallelTaskFrame;
+    pub use crate::task::dependencyframe::DependencyTaskFrame;
+    pub use crate::task::dynamicframe::DynamicTaskFrame;
+
+    // Scheduling
+    pub use crate::task::schedule::TaskScheduleInterval;
+    pub use crate::task::schedule::cron::TaskScheduleCron;
+    pub use crate::task::schedule::calendar::TaskScheduleCalendar;
+    pub use crate::task::scheduling_strats::{
+        SequentialSchedulingPolicy,
+        ConcurrentSchedulingPolicy,
+        CancelPreviousSchedulingPolicy,
+        CancelCurrentSchedulingPolicy,
+    };
+
+    // TaskHooks / TaskHookEvents
+    pub use crate::task::hooks::{
+        TaskHook,
+        NonObserverTaskHook,
+        events::*
+    };
+
+    // Utils / Misc
+    pub use crate::task::TaskFrameBuilder;
+    pub use crate::task::dependency::*;
+    pub use crate::task::retryframe::{
+        RetryBackoffStrategy,
+        ExponentialBackoffStrategy
+    };
+} // skipcq: RS-D1001
