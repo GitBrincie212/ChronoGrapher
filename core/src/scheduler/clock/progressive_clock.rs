f5c2b65089fcd01476f04e09fe8ab03039d24821
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-12-27 18:06:42 +0200

    Renamed the module from system_clock to ProgressiveClock and rewrote it a bit due to that

---
+++ core/src/scheduler/clock/progressive_clock.rs
@@ -0,0 +1,57 @@
+use std::marker::PhantomData;
+use crate::scheduler::clock::SchedulerClock;
+use async_trait::async_trait;
+
+#[allow(unused_imports)]
+use crate::scheduler::clock::VirtualClock;
+use crate::utils::Timestamp;
+
+/// [`ProgressiveClock`] is an implementation of [`SchedulerClock`] trait, it is the default option
+/// for scheduling, unlike [`VirtualClock`], it moves forward no matter what and cannot be advanced
+/// at any arbitrary point (due to its design)
+///
+/// # Constructor(s)
+/// One can simply use the default rust's struct initialization or via [`ProgressiveClock::default`]
+/// to construct the [`ProgressiveClock`]
+///
+/// # Trait Implementation(s)
+/// While [`ProgressiveClock`] implements the [`SchedulerClock`] trait, it also implements the
+/// [`Default`] trait, the [`Clone`] trait and the [`Copy`] trait
+///
+/// # See Also
+/// - [`VirtualClock`]
+/// - [`SchedulerClock`]
+pub struct ProgressiveClock<T: Timestamp>(PhantomData<T>);
+
+impl<T: Timestamp> Default for ProgressiveClock<T> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<T: Timestamp> Clone for ProgressiveClock<T> {
+    fn clone(&self) -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<T: Timestamp> Copy for ProgressiveClock<T> {}
+
+#[async_trait]
+impl<T: Timestamp> SchedulerClock<T> for ProgressiveClock<T> {
+    async fn now(&self) -> T {
+        T::now()
+    }
+
+    async fn idle_to(&self, to: T) {
+        let now = T::now();
+        let duration = match to.duration_since(now) {
+            Some(duration) => duration,
+            None => {
+                return;
+            }
+        };
+
+        tokio::time::sleep(duration).await;
+    }
+}
