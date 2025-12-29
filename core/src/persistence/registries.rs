be3cf23f3f63ed1e3722d3161fb9da045bec8617
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-12-29 12:28:36 +0200

    A couple of changes have been made to the registry-based system (the registry system might get a rewrite as it proves unscalable)

--- core/src/persistence/registries.rs
+++ core/src/persistence/registries.rs
@@ -2,18 +2,20 @@ use crate::persistence::PersistenceObject;
 use dashmap::DashMap;
 use erased_serde::Deserializer as ErasedDeserializer;
 use erased_serde::Serializer as ErasedSerializer;
-use serde::ser::{Error as SerializeError, SerializeStruct};
-use serde::{Serialize, Serializer};
+use serde::ser::{Error as SerializeError};
 use std::any::{Any, TypeId};
 use std::sync::{Arc, LazyLock};
+
+pub type AnySerializeFunc = fn(&dyn Any, &mut dyn ErasedSerializer) -> Result<(), erased_serde::Error>;
+pub type AnyDeserializeFunc = fn(&mut dyn ErasedDeserializer<'_>) -> Result<Arc<dyn Any>, erased_serde::Error>;
 
 pub static PERSISTENCE_REGISTRIES: LazyLock<PersistenceRegistriesManager> =
     LazyLock::new(|| PersistenceRegistriesManager::default());
 
+#[derive(Clone, Copy)]
 pub struct ErasedPersistenceEntry {
-    pub serialize: fn(&dyn Any, &mut dyn ErasedSerializer) -> Result<(), erased_serde::Error>,
-    pub deserialize:
-        fn(&mut dyn ErasedDeserializer<'_>) -> Result<Arc<dyn Any>, erased_serde::Error>,
+    pub serialize: AnySerializeFunc,
+    pub deserialize: AnyDeserializeFunc
 }
 
 #[derive(Default)]
@@ -23,7 +25,7 @@ pub struct PersistenceRegistriesManager {
 }
 
 impl PersistenceRegistriesManager {
-    pub fn register<T: PersistenceObject + 'static>(&self) {
+    pub fn register<T: PersistenceObject>(&self) {
         self.persistent_registry.insert(
             T::PERSISTENCE_ID,
             ErasedPersistenceEntry {
@@ -43,5 +45,15 @@ impl PersistenceRegistriesManager {
 
         self.runtime_registry
             .insert(TypeId::of::<T>(), T::PERSISTENCE_ID);
+    }
+
+    pub fn get_hook_id(&self, type_id: &TypeId) -> Option<&'static str> {
+        let id = self.runtime_registry.get(type_id)?;
+        Some(id.value())
+    }
+
+    pub fn get_hook_entry(&self, hook_id: &'static str) -> Option<ErasedPersistenceEntry> {
+        let entry = self.persistent_registry.get(hook_id)?;
+        Some(*entry.value())
     }
 }
