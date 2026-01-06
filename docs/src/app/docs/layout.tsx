28bbd8beb65ac1d715c107606c7983c83242fa11
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-01-06 12:31:57 +0200

    Added a programming language toggle, to switch to multiple programming languages (docs change)

--- docs/src/app/docs/layout.tsx
+++ docs/src/app/docs/layout.tsx
@@ -1,10 +1,15 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import ProgrammingLangToggle from "@/components/ui/toggle-language";
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout tree={source.getPageTree()} sidebar={{
+        banner: (
+            <ProgrammingLangToggle />
+        ),
+    }} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
