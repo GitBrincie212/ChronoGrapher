00c56770a5ebc61a3bdc9f87257db6257d2ddcd5
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-01-09 22:33:20 +0200

    Fixed some of the complaints from BiomeJS

--- docs/src/app/docs/layout.tsx
+++ docs/src/app/docs/layout.tsx
@@ -1,15 +1,17 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import ProgrammingLangToggle from "@/components/ui/toggle-language";
+import { baseOptions } from "@/lib/layout.shared";
+import { source } from "@/lib/source";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.getPageTree()} sidebar={{
-        banner: (
-            <ProgrammingLangToggle />
-        ),
-    }} {...baseOptions()}>
+    <DocsLayout
+      tree={source.getPageTree()}
+      sidebar={{
+        banner: <ProgrammingLangToggle />,
+      }}
+      {...baseOptions()}
+    >
       {children}
     </DocsLayout>
   );
