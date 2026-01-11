35c7c8cc32b44f8d92aee5845dc5bd18ee5840a8
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-01-11 11:23:37 +0200

    Added a SharedHook, used to wrap types and manage state from there

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -4,6 +4,10 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use uuid::Uuid;
+use crate::prelude::NonObserverTaskHook;
+
+pub struct SharedHook<T: Send + Sync + 'static>(pub T);
+impl<T: Send + Sync + 'static> NonObserverTaskHook for SharedHook<T> {}
 
 #[macro_export]
 macro_rules! define_event_group {
@@ -37,6 +41,16 @@ macro_rules! define_event {
             const EVENT_ID: &'static str = concat!("chronographer_core#", stringify!($name));
         }
     };
+}
+
+#[macro_export]
+macro_rules! create_shared {
+    ($ctx: expr => $init:expr => $block:expr) => {{
+        let shared = Arc::new(SharedHook(Arc::new($init)));
+        $ctx.attach_hook::<()>(shared.clone());
+        $block(shared.0.clone()).await?;
+        $ctx.detach_hook::<(), SharedHook<_>>();
+    }};
 }
 
 pub trait Timestamp: Clone + Ord + Send + Sync + 'static {
