6225ea56eb2de54f275b466d6d47a339af448da7
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-01-12 23:18:44 +0200

    Removed the Timestamp, removed the internal datetime to sys time helpers and added a trait constraint for TaskIdentifier

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,9 +1,7 @@
 use crate::prelude::NonObserverTaskHook;
-use async_trait::async_trait;
-use chrono::{DateTime, Datelike, Local, TimeZone, Timelike};
 use std::fmt::Debug;
 use std::hash::Hash;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::time::Duration;
 use uuid::Uuid;
 
 pub struct SharedHook<T: Send + Sync + 'static>(pub T);
@@ -65,84 +63,12 @@ pub trait Timestamp: Clone + Ord + Send + Sync + 'static {
     fn millisecond(&self) -> u16;
 }
 
-impl Timestamp for SystemTime {
-    fn now() -> Self {
-        SystemTime::now()
-    }
-
-    fn duration_since(&self, earlier: Self) -> Option<Duration> {
-        self.duration_since(earlier).ok()
-    }
-
-    fn year(&self) -> u32 {
-        system_time_to_date_time(self).year() as u32
-    }
-
-    fn month(&self) -> u8 {
-        system_time_to_date_time(self).month0() as u8
-    }
-
-    fn day(&self) -> u8 {
-        system_time_to_date_time(self).day0() as u8
-    }
-
-    fn hour(&self) -> u8 {
-        system_time_to_date_time(self).hour() as u8
-    }
-
-    fn minute(&self) -> u8 {
-        system_time_to_date_time(self).minute() as u8
-    }
-
-    fn second(&self) -> u8 {
-        system_time_to_date_time(self).second() as u8
-    }
-
-    fn millisecond(&self) -> u16 {
-        system_time_to_date_time(self).timestamp_subsec_millis() as u16
-    }
-}
-
-pub trait TaskIdentifier: 'static + Debug + Clone + Eq + Hash + Send + Sync {
+pub trait TaskIdentifier: 'static + Debug + Clone + Eq + PartialEq<Self> + Hash + Send + Sync {
     fn generate() -> Self;
 }
 
 impl TaskIdentifier for Uuid {
     fn generate() -> Self {
         Uuid::new_v4()
-    }
-}
-
-#[async_trait]
-pub trait RescheduleAlerter: 'static + Send + Sync {
-    async fn notify_task_finish(&self);
-}
-
-/// Simply converts the ``SystemTime`` to a ``DateTime<Local>``, it is a private
-/// method used internally by ChronoGrapher, as such why it lives in utils module
-pub(crate) fn system_time_to_date_time(t: &SystemTime) -> DateTime<Local> {
-    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
-        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
-        Err(e) => {
-            let dur = e.duration();
-            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
-            if nsec == 0 {
-                (-sec, 0)
-            } else {
-                (-sec - 1, 1_000_000_000 - nsec)
-            }
-        }
-    };
-    Local.timestamp_opt(sec, nsec).unwrap()
-}
-
-/// Simply converts the ``DateTime<Local>`` to a ``SystemTime``, it is a private
-/// method used internally by ChronoGrapher, as such why it lives in utils module
-pub(crate) fn date_time_to_system_time(dt: DateTime<impl TimeZone>) -> SystemTime {
-    let duration_since_epoch = dt.timestamp_nanos_opt().unwrap();
-    if duration_since_epoch >= 0 {
-        UNIX_EPOCH + Duration::from_nanos(duration_since_epoch as u64)
-    } else {
-        UNIX_EPOCH - Duration::from_nanos((-duration_since_epoch) as u64)
     }
 }
