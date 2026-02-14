1019674364f4b40f6fa791a69992043ad72eb295
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:56:38 +0200

    Tweaked the DefaultSchedulerEngine in accordance with the recent changes

--- core/src/scheduler/engine/default.rs
+++ core/src/scheduler/engine/default.rs
@@ -3,9 +3,7 @@ use crate::scheduler::clock::SchedulerClock;
 use crate::scheduler::engine::SchedulerEngine;
 use crate::scheduler::task_dispatcher::{EngineNotifier, SchedulerTaskDispatcher};
 use crate::scheduler::task_store::SchedulerTaskStore;
-use crate::task::DynArcError;
 use async_trait::async_trait;
-use std::any::Any;
 use std::sync::Arc;
 use tokio::join;
 
@@ -21,16 +19,14 @@ impl<C: SchedulerConfig> SchedulerEngine<C> for DefaultSchedulerEngine {
         dispatcher: Arc<C::SchedulerTaskDispatcher>,
     ) {
         let (scheduler_send, mut scheduler_receive) =
-            tokio::sync::mpsc::channel::<(Box<dyn Any + Send + Sync>, Option<DynArcError>)>(1024);
+            tokio::sync::mpsc::channel::<(C::TaskIdentifier, Option<C::Error>)>(1024);
         let notifier = tokio::sync::Notify::new();
         join!(
             async {
                 while let Some((id, err)) = scheduler_receive.recv().await {
-                    let id = id.downcast_ref::<C::TaskIdentifier>()
-                        .expect("Different type was used on EngineNotifier, which was meant as for an identifier");
                     match err {
                         None => {
-                            if let Some(_task) = store.get(id).await {
+                            if let Some(_task) = store.get(&id).await {
                                 /*
                                 if let Some(max_runs) = task.max_runs()
                                     && task.runs() >= max_runs.get()
@@ -38,7 +34,7 @@ impl<C: SchedulerConfig> SchedulerEngine<C> for DefaultSchedulerEngine {
                                     continue;
                                 }
                                  */
-                                store.reschedule(&clock, id).await.unwrap_or_else(|_| {
+                                store.reschedule(&clock, &id).await.unwrap_or_else(|_| {
                                     panic!("Failed to reschedule Task with the identifier {id:?}")
                                 });
                                 notifier.notify_waiters();
@@ -61,7 +57,7 @@ impl<C: SchedulerConfig> SchedulerEngine<C> for DefaultSchedulerEngine {
                             _ = clock.idle_to(time) => {
                                 store.pop().await;
                                 if !store.exists(&id).await { continue; }
-                                let sender = EngineNotifier::new::<C>(
+                                let sender = EngineNotifier::new(
                                     id,
                                     scheduler_send.clone()
                                 );
