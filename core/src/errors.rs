c830292f70d11b5e97f0fe037f5badde1d35353b
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:49:54 +0200

    [HEAVY WIP] Significantly reworked the way ChronoGrapher organizes its errors in accordance with the new error handling approach

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -1,44 +1,80 @@
-use std::fmt::Debug;
+use std::ops::Deref;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::time::Duration;
 use thiserror::Error;
 
-#[allow(unused_imports)]
-use crate::task::SelectTaskFrame;
-
-#[allow(unused_imports)]
-use crate::task::ConditionalFrame;
-
-#[allow(unused_imports)]
-use crate::task::DependencyTaskFrame;
-
-#[allow(unused_imports)]
-use crate::task::TimeoutTaskFrame;
-
-#[allow(unused_imports)]
-use crate::task::dependencyframe::DependentFailureOnFail;
-
-/// [`ChronographerErrors`] is the main enum that contains all the errors which can be thrown by
-/// ChronoGrapher, it uses under the hood [`thiserror`] to make it as smooth sailing to add more
-/// errors in the future as possible. This enum is private only to the core package itself,
-/// as no other is aware of the existence
 #[derive(Error, Debug)]
-pub enum ChronographerErrors {
-    /// This error is meant to happen when retrieving an index from a container that has a
-    /// specified length, but the index is out of bounds. In the core package it is mainly caused
-    /// by [`SelectTaskFrame`]
+pub enum ConditionalTaskFrameError<T1: Error + Send + Sync + 'static, T2: Error + Send + Sync + 'static> {
+    #[error("ConditionalTaskFrame has failed, with the error originating from primary TaskFrame's failure:\n\t{0}")]
+    PrimaryFailed(T1),
+
+    #[error("ConditionalTaskFrame has failed, with the error originating from secondary TaskFrame's failure:\n\t{0}")]
+    SecondaryFailed(T2),
+
+    #[error("ConditionalTaskFrame has returned false with `error_on_false` enabled")]
+    TaskConditionFail
+}
+
+#[derive(Error, Debug)]
+pub enum TimeoutTaskFrameError<T: Error + Send + Sync + 'static> {
+    #[error("TimeoutTaskFrame has failed, with the error originating from primary TaskFrame's failure:\n\t{0}")]
+    Inner(T),
+
+    #[error("TimeoutTaskFrame has timeout with max duration '{0:?}'")]
+    Timeout(Duration)
+}
+
+macro_rules! newtype_error {
+    ($name: ident) => {
+        #[derive(Debug)]
+        pub struct $name<T: Error + Send + Sync + 'static> (T);
+        impl<T: Error + Send + Sync + 'static> $name<T> {
+            pub fn new(error: T) -> Self {
+                Self(error)
+            }
+        }
+
+        impl<T: Error + Send + Sync + 'static> Display for $name<T> {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                Display::fmt(&self.0, f)
+            }
+        }
+
+        impl<T: Error + Send + Sync + 'static> Error for $name<T> {
+            fn source(&self) -> Option<&(dyn Error + 'static)> {
+                self.0.source()
+            }
+        }
+
+        impl<T: Error + Send + Sync + 'static> Deref for $name<T> {
+            type Target = T;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+    };
+}
+
+newtype_error!(DelayTaskFrameError);
+newtype_error!(FallbackTaskFrameError);
+newtype_error!(RetriableTaskFrameError);
+newtype_error!(ThresholdTaskFrameError);
+newtype_error!(DependencyTaskFrameError);
+
+#[derive(Error, Debug)]
+pub enum StandardCoreErrorsCG {
     #[error(
         "Task frame index `{0}` is out of bounds for `{1}` with task frame size `{2}` element(s)"
     )]
     TaskIndexOutOfBounds(usize, String, usize),
 
-    /// This error is meant to happen when [`ConditionalTaskFrame`]
-    /// returns true and the flag ``error_on_false`` is set to true
     #[error(
         "ConditionalTaskFrame returned false with error_on_false set to true, as such this error returns"
     )]
     TaskConditionFail,
 
-    /// This error is meant to happen when dependencies from [`DependencyTaskFrame`]
-    /// aren't resolved with the dependent behavior set to [`DependentFailureOnFail`]
     #[error("Dependencies have not been resolved")]
     TaskDependenciesUnresolved,
 
