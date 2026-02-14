92f92e6f13224d3626544ace7b0d28f87cd3f9da
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:35:47 +0200

    [HEAVY WIP] Unified the ParallelTakFrame, SequentialTaskFrame and SelectTaskFrame as the CollectionTaskFrame with a pluggable strategy

---
+++ core/src/task/frames/collectionframe.rs
@@ -0,0 +1,195 @@
+use crate::prelude::TaskHookEvent;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+#[allow(unused_imports)]
+use crate::task::{RestrictTaskFrameContext, TaskFrame};
+use async_trait::async_trait;
+use std::sync::Arc;
+use crate::{define_event, define_event_group};
+use crate::task::{DynArcError, ErasedTaskFrame, TaskFrameContext};
+/* === SEQUENTIAL CODE ===
+        for taskframe in self.tasks.iter() {
+            ctx.emit::<OnChildTaskFrameStart>(&()).await; // skipcq: RS-E1015
+            let result = ctx.subdivide(taskframe.clone()).await.err();
+            ctx.emit::<OnChildTaskFrameEnd>(&result.clone()).await;
+            match self.policy.should_quit(result).await {
+                ConsensusGTFE::SkipResult => {}
+                ConsensusGTFE::ReturnSuccess => break,
+                ConsensusGTFE::ReturnError(err) => return Err(err),
+            }
+        }
+        Ok(())
+ */
+
+/* === SELECTOR CODE ===
+    #[async_trait]
+    pub trait SelectFrameAccessor: Send + Sync {
+        async fn select(&self, ctx: &TaskFrameContext) -> usize;
+    }
+
+    let idx = self.accessor.select(ctx).await;
+    if let Some(frame) = self.frames.get(idx) {
+        ctx.emit::<OnTaskFrameSelection>(&(idx, frame.clone()))
+            .await;
+        return ctx.subdivide(frame.clone()).await;
+    }
+
+    Err(Arc::new(StandardCoreErrorsCG::TaskIndexOutOfBounds(
+        idx,
+        "SelectTaskFrame".to_owned(),
+        self.frames.len(),
+    )))
+ */
+
+/* === PARALLEL CODE ===
+        let mut js = tokio::task::JoinSet::new();
+
+        for frame in self.tasks.iter() {
+            let frame_clone = frame.clone();
+            let ctx_clone = ctx.clone();
+            js.spawn(async move {
+                ctx_clone.emit::<OnChildTaskFrameStart>(&()).await; // skipcq: RS-E1015
+                let result = ctx_clone.subdivide(frame_clone.clone()).await;
+                ctx_clone
+                    .emit::<OnChildTaskFrameEnd>(&result.clone().err())
+                    .await; // skipcq: RS-E1015
+                result
+            });
+        }
+
+        while let Some(result) = js.join_next().await {
+            let Ok(k) = result else { continue };
+
+            match self.policy.should_quit(k.err()).await {
+                ConsensusGTFE::SkipResult => continue,
+                ConsensusGTFE::ReturnSuccess => break,
+                ConsensusGTFE::ReturnError(err) => return Err(err),
+            }
+        }
+
+        Ok(())
+ */
+
+#[derive(Debug)]
+pub struct CollectionTaskError {
+    index: usize,
+    error: Box<dyn Error + Send + Sync>
+}
+
+
+impl Display for CollectionTaskError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(
+            &format!("{} inside CollectionTaskFrame at index {}", &self.error, self.index)
+        )
+    }
+}
+
+impl Error for CollectionTaskError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.error.source()
+    }
+}
+
+#[async_trait]
+pub trait CollectionExecStrategy: Send + Sync + Sized + 'static {
+    async fn execute(
+        &self,
+        handle: CollectionTaskFrameHandle<'_, Self>
+    ) -> Result<(), <CollectionTaskFrame<Self> as TaskFrame>::Error>;
+}
+
+pub enum ConsensusGTFE<T: Error + Send + Sync + 'static> {
+    SkipResult,
+    ReturnError(T),
+    ReturnSuccess,
+}
+
+#[async_trait]
+pub trait CollectionExecPolicy<T: Error + Send + Sync + 'static>: Send + Sync {
+    async fn should_quit(&self, result: Option<T>) -> ConsensusGTFE<T>;
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct GroupedTaskFramesQuitOnSuccess;
+
+#[async_trait]
+impl<T: Error + Send + Sync + 'static> CollectionExecPolicy<T> for GroupedTaskFramesQuitOnSuccess {
+    async fn should_quit(&self, result: Option<T>) -> ConsensusGTFE<T> {
+        match result {
+            None => ConsensusGTFE::ReturnSuccess,
+            Some(_) => ConsensusGTFE::SkipResult,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct GroupedTaskFramesQuitOnFailure;
+
+#[async_trait]
+impl<T: Error + Send + Sync + 'static> CollectionExecPolicy<T> for GroupedTaskFramesQuitOnFailure {
+    async fn should_quit(&self, result: Option<T>) -> ConsensusGTFE<T> {
+        match result {
+            None => ConsensusGTFE::SkipResult,
+            Some(err) => ConsensusGTFE::ReturnError(err),
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct GroupedTaskFramesSilent;
+
+#[async_trait]
+impl<T: Error + Send + Sync + 'static> CollectionExecPolicy<T> for GroupedTaskFramesSilent {
+    async fn should_quit(&self, _result: Option<T>) -> ConsensusGTFE<T> {
+        ConsensusGTFE::SkipResult
+    }
+}
+
+define_event!(
+    OnChildTaskFrameStart, ()
+);
+
+define_event!(
+    OnChildTaskFrameEnd, Option<&'a (dyn Error + Send + Sync)>
+);
+
+define_event_group!(
+    ChildTaskFrameEvents,
+    OnChildTaskFrameStart, OnChildTaskFrameEnd
+);
+
+
+pub struct CollectionTaskFrame<T: CollectionExecStrategy> {
+    taskframes: Vec<Arc<dyn ErasedTaskFrame>>,
+    strategy: T,
+}
+
+pub struct CollectionTaskFrameHandle<'a, T: CollectionExecStrategy> {
+    collection: &'a CollectionTaskFrame<T>,
+    ctx: &'a TaskFrameContext<'a>
+}
+
+impl<'a, T: CollectionExecStrategy> CollectionTaskFrameHandle<'a, T> {
+    pub async fn run(&self, idx: usize) -> Option<DynArcError> {
+        let taskframe = self.collection.taskframes[idx].as_ref();
+        self.ctx.emit::<OnChildTaskFrameStart>(&()).await; // skipcq: RS-E1015
+        let result = self.ctx.erased_subdivide(taskframe).await.err();
+        self.ctx.emit::<OnChildTaskFrameEnd>(&result.as_ref().map(|x| x.as_ref())).await;
+        result
+    }
+}
+
+#[async_trait]
+impl<T: CollectionExecStrategy> TaskFrame for CollectionTaskFrame<T> {
+    type Error = CollectionTaskError;
+
+    async fn execute(&self, ctx: &TaskFrameContext) -> Result<(), Self::Error> {
+        let handle = CollectionTaskFrameHandle {
+            collection: self,
+            ctx
+        };
+        
+        self.strategy.execute(handle).await
+    }
+}
