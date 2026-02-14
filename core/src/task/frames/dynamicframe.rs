49c6ea4eb954a58cb6cfe4da7e7a391a06e4f577
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:48:57 +0200

    Changed significantly the DynamicTaskFrame in accordance with the recent changes in the error handling approach

--- core/src/task/frames/dynamicframe.rs
+++ core/src/task/frames/dynamicframe.rs
@@ -1,40 +1,14 @@
-use crate::task::{TaskContext, DynArcError, TaskFrame};
+use std::error::Error;
+use crate::task::{TaskFrame, TaskFrameContext};
 use async_trait::async_trait;
 
-/// Represents a **no-operation task frame** that does nothing. This task frame type
-/// acts as a **leaf node** within the task frame hierarchy. Its primary role is to
-/// represent a hollow task frame that has no operations
-///
-/// This is useful for skipping execution of a task frame that is required, making it effectively
-/// just a placeholder (that is why it is a no-operation task frame)
-///
-/// # Constructor(s)
-/// When constructing a [`DynamicTaskFrame`], one can use the default rust struct initialization,
-/// or they can use [`DynamicTaskFrame::default`] via [`Default`]
-///
-/// # Events
-/// When it comes to events, [`DynamicTaskFrame`], it has no local task frame events
-///
-/// # Trait Implementation(s)
-/// It is obvious that the [`DynamicTaskFrame`] implements [`TaskFrame`] since this
-/// is a part of the default provided implementations, but also:
-/// - [`Debug`] (just prints the name, nothing more, nothing less)
-/// - [`Clone`]
-/// - [`Copy`]
-/// - [`Default`]
-/// - [`PersistenceObject`]
-/// - [`Serialize`]
-/// - [`Deserialize`]
-///
-/// # See Also
-/// - [`TaskFrame`]
-/// - [`DynamicTaskFrame::default`]
 pub struct DynamicTaskFrame<T>(T);
 
-impl<T, F> DynamicTaskFrame<T>
+impl<T, F, E> DynamicTaskFrame<T>
 where
-    T: (Fn(&TaskContext) -> F) + Send + Sync + 'static,
-    F: Future<Output = Result<(), DynArcError>> + Send + 'static,
+    T: (Fn(&TaskFrameContext) -> F) + Send + Sync + 'static,
+    F: Future<Output = Result<(), E>> + Send + 'static,
+    E: Error + Send + Sync + 'static
 {
     pub fn new(func: T) -> Self {
         Self(func)
@@ -42,12 +16,15 @@ where
 }
 
 #[async_trait]
-impl<T, F> TaskFrame for DynamicTaskFrame<T>
+impl<T, F, E> TaskFrame for DynamicTaskFrame<T>
 where
-    T: (Fn(&TaskContext) -> F) + Send + Sync + 'static,
-    F: Future<Output = Result<(), DynArcError>> + Send + 'static,
+    T: (Fn(&TaskFrameContext) -> F) + Send + Sync + 'static,
+    F: Future<Output = Result<(), E>> + Send + 'static,
+    E: Error + Send + Sync + 'static
 {
-    async fn execute(&self, ctx: &TaskContext) -> Result<(), DynArcError> {
+    type Error = E;
+
+    async fn execute(&self, ctx: &TaskFrameContext) -> Result<(), Self::Error> {
         self.0(ctx).await
     }
 }
