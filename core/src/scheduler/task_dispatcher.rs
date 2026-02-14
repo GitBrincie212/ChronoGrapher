5345e83439871d99263da37f9924020bd888d759
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:47:21 +0200

    Changed the SchedulerTaskDispatcher in accordance with the new error handling approach as well as the new requirements, made the EngineNotifier more type-safe by using the SchedulerConfig generic

--- core/src/scheduler/task_dispatcher.rs
+++ core/src/scheduler/task_dispatcher.rs
@@ -1,32 +1,31 @@
 pub mod default; // skipcq: RS-D1001
 
 pub use default::*;
-use std::any::Any;
 
 #[allow(unused_imports)]
 use crate::scheduler::Scheduler;
 use crate::scheduler::SchedulerConfig;
-use crate::task::{ErasedTask, DynArcError};
+use crate::task::ErasedTask;
 use async_trait::async_trait;
 use std::sync::Arc;
 
-pub struct EngineNotifier {
-    id: Box<dyn Any + Send + Sync>,
-    notify: tokio::sync::mpsc::Sender<(Box<dyn Any + Send + Sync>, Option<DynArcError>)>,
+pub struct EngineNotifier<C: SchedulerConfig> {
+    id: C::TaskIdentifier,
+    notify: tokio::sync::mpsc::Sender<(C::TaskIdentifier, Option<C::Error>)>,
 }
 
-impl EngineNotifier {
-    pub fn new<C: SchedulerConfig>(
+impl<C: SchedulerConfig> EngineNotifier<C> {
+    pub fn new(
         id: C::TaskIdentifier,
-        notify: tokio::sync::mpsc::Sender<(Box<dyn Any + Send + Sync>, Option<DynArcError>)>,
+        notify: tokio::sync::mpsc::Sender<(C::TaskIdentifier, Option<C::Error>)>,
     ) -> Self {
         Self {
-            id: Box::new(id),
+            id,
             notify,
         }
     }
 
-    pub async fn notify(self, result: Option<DynArcError>) {
+    pub async fn notify(self, result: Option<C::Error>) {
         self.notify
             .send((self.id, result))
             .await
@@ -34,43 +33,9 @@ impl EngineNotifier {
     }
 }
 
-/// [`SchedulerTaskDispatcher`] is a trait for implementing a scheduler task dispatcher. It acts as
-/// a central point for when a task wants to execute, it does not handle scheduling, in fact it
-/// communicates closely to the [`Scheduler`] to stay informed on whenever to execute the task
-///
-/// # Required Method(s)
-/// When implementing the [`SchedulerTaskDispatcher`], one has to supply an implementation for
-/// the method [`SchedulerTaskDispatcher::dispatch`] which is where the dispatching logic lives
-///
-/// # Trait Implementation(s)
-/// There is one implementation in particular for [`SchedulerTaskDispatcher`], that is [`DefaultTaskDispatcher`]
-/// which has a thread pool that handles work-stealing and priority management to ensure that
-/// ChronoGrapher stays responsive even under heavy workflow
-///
-/// # Object Safety
-/// [`SchedulerTaskDispatcher`] is object safe as seen throughout [`Scheduler`] source code
-///
-/// # See Also
-/// - [`SchedulerTaskDispatcher::dispatch`]
-/// - [`Scheduler`]
-/// - [`DefaultTaskDispatcher`]
 #[async_trait]
 pub trait SchedulerTaskDispatcher<C: SchedulerConfig>: 'static + Send + Sync {
     async fn init(&self) {}
 
-    /// The main logic of the [`SchedulerTaskDispatcher`]. This is where it handles
-    /// how to execute a specified task and notify the [`Scheduler`] accordingly
-    ///
-    /// # Argument(s)
-    /// The method accepts 4 arguments, those being a ``sender`` for notifying the
-    /// [`Scheduler`] when the [`Task`] has finished executing, an ``emitter`` which is
-    /// for event emission (the dispatcher by itself doesn't emit events. It is used
-    /// as an argument for running a task), a [`Task`] represented as ``task`` and an
-    /// index pointing to the [`Task`]
-    ///
-    /// # See Also
-    /// - [`Task`]
-    /// - [`Scheduler`]
-    /// - [`SchedulerTaskDispatcher`]
-    async fn dispatch(&self, task: Arc<ErasedTask>, rescheduler_notifier: EngineNotifier);
+    async fn dispatch(&self, task: Arc<ErasedTask<C::Error>>, rescheduler_notifier: EngineNotifier<C>);
 }
