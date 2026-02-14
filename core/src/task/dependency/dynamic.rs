b6bcf83343c8f2c3a0cc68f199709d9a44bf04f9
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-14 14:55:12 +0200

    Removed the API documentation from DynamicDependency

--- core/src/task/dependency/dynamic.rs
+++ core/src/task/dependency/dynamic.rs
@@ -6,56 +6,12 @@ use std::sync::atomic::{AtomicBool, Ordering};
 
 type DynamicFunction = Arc<dyn Fn() -> Pin<Box<dyn Future<Output = bool> + Send>> + Send + Sync>;
 
-/// [`DynamicDependency`] represents a dependency which hosts a function inside that computes
-/// if the dependency is resolved (unlike most dependencies which use a caching mechanism)
-///
-/// # Usage Note(s)
-/// this dependency type cannot persist disk due to the nature of functions. For those cases, it is
-/// advised to create a struct yourself and implement the trait [`FrameDependency`]
-///
-/// In addition, Due to the nature of being a function with no caching mechanism, it cannot be manually
-/// resolved or unresolved (tho it can still be disabled). As such, while it is provided in the core,
-/// this shouldn't be used apart from debugging, experimenting and other niche cases
-///
-/// # Constructor(s)
-/// When constructing a [`DynamicDependency`], one can use [`DynamicDependency::new`] to do
-/// so with a supplied function
-///
-/// # Trait Implementation(s)
-/// It is clear as day that [`DynamicDependency`] implements the [`FrameDependency`]
-///
-/// # Example
-/// ```ignore
-/// use chronographer_core::task::dependency::DynamicDependency;
-///
-/// let dependency = DynamicDependency::new(|_| async {
-///     println!("Bip boop, i compute your value!");
-///     true // This is just an example
-/// });
-///
-/// // You can then attach it to a DependencyTaskFrame
-/// ```
-///
-/// # See Also
-/// - [`FrameDependency`]
-/// - [`DynamicDependency::new`]
 pub struct DynamicDependency {
     func: DynamicFunction,
     is_enabled: Arc<AtomicBool>,
 }
 
 impl DynamicDependency {
-    /// Creates / Constructs a new [`DynamicDependency`] instance
-    ///
-    /// # Argument(s)
-    /// This method accepts one single argument, that being ``func``, which is
-    /// a function to wrap [`DynamicDependency`]
-    ///
-    /// # Returns
-    /// The newly created [`DynamicDependency`] with the function being ``func``
-    ///
-    /// # See Also
-    /// - [`DynamicDependency`]
     pub fn new<Fut, Func>(func: Func) -> Self
     where
         Fut: Future<Output = bool> + Send + 'static,
