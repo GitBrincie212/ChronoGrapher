b292909a40ee6515f2aeb52e7eb98f158daaba68
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-21 19:14:39 +0200

    Removed some useless utilities

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,10 +1,6 @@
-use crate::prelude::NonObserverTaskHook;
 use std::fmt::Debug;
 use std::hash::Hash;
 use uuid::Uuid;
-
-pub struct SharedHook<T: Send + Sync + 'static>(pub T);
-impl<T: Send + Sync + 'static> NonObserverTaskHook for SharedHook<T> {}
 
 #[macro_export]
 macro_rules! define_event_group {
@@ -41,15 +37,6 @@ macro_rules! define_event {
     };
 }
 
-#[macro_export]
-macro_rules! create_shared {
-    ($ctx: expr => $init:expr => $block:expr) => {{
-        let shared = Arc::new(SharedHook(Arc::new($init)));
-        $ctx.attach_hook::<()>(shared.clone());
-        $block(shared.0.clone()).await?;
-        $ctx.detach_hook::<(), SharedHook<_>>();
-    }};
-}
 pub trait TaskIdentifier:
     'static + Debug + Clone + Eq + PartialEq<Self> + Hash + Send + Sync
 {
