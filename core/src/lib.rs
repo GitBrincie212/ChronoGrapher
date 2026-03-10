455ee25baa9ed6c13a25922a969eb7ea6fe3c951
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-03-10 12:52:00 +0200

    Added the core crate which re-exports everything from base and proc macros crate

---
+++ core/src/lib.rs
@@ -0,0 +1,55 @@
+pub use chronographer_base::*;
+pub use chronographer_macros::*;
+
+pub mod prelude {
+    // Core
+    pub use crate::errors::TaskError;
+    pub use crate::task::{TaskFrameContext, RestrictTaskFrameContext, Task};
+
+    // Common frames
+    pub use crate::task::collectionframe::CollectionTaskFrame;
+    pub use crate::task::collectionframe::GroupedTaskFramesQuitOnFailure;
+    pub use crate::task::collectionframe::GroupedTaskFramesQuitOnSuccess;
+    pub use crate::task::collectionframe::GroupedTaskFramesSilent;
+    pub use crate::task::collectionframe::ParallelExecStrategy;
+    pub use crate::task::collectionframe::SelectFrameAccessor;
+    pub use crate::task::collectionframe::SelectionExecStrategy;
+    pub use crate::task::collectionframe::SequentialExecStrategy;
+    pub use crate::task::delayframe::DelayTaskFrame;
+    pub use crate::task::dependencyframe::DependencyTaskFrame;
+    pub use crate::task::dynamicframe::DynamicTaskFrame;
+    pub use crate::task::fallbackframe::FallbackTaskFrame;
+    pub use crate::task::retryframe::RetriableTaskFrame;
+    pub use crate::task::timeoutframe::TimeoutTaskFrame;
+
+    // Scheduling / Triggering
+    pub use crate::task::trigger::TaskScheduleInterval;
+    pub use crate::task::trigger::schedule::calendar::TaskScheduleCalendar;
+    pub use crate::task::trigger::schedule::calendar::TaskCalendarField;
+    pub use crate::task::trigger::schedule::cron::TaskScheduleCron;
+    pub use crate::task::trigger::schedule::TaskSchedule;
+    pub use crate::task::trigger::TaskTrigger;
+
+    // Schedulers
+    pub use crate::scheduler::DefaultScheduler;
+    pub use crate::scheduler::DefaultSchedulerConfig;
+    pub use crate::scheduler::Scheduler;
+    pub use crate::scheduler::SchedulerConfig;
+
+    #[cfg(feature = "anyhow")]
+    pub use crate::scheduler::DefaultAnyhowScheduler;
+
+    #[cfg(feature = "eyre")]
+    pub use crate::scheduler::DefaultEyreScheduler;
+
+    // TaskHooks / TaskHookEvents
+    pub use crate::task::hooks::{NonObserverTaskHook, TaskHook, events::*};
+
+    // Utils / Misc
+    pub use crate::task::TaskFrameBuilder;
+    pub use crate::task::dependency::*;
+    pub use crate::task::retryframe::{
+        ExponentialBackoffStrategy, LinearBackoffStrategy, ConstantBackoffStrategy,
+        JitterBackoffStrategy, RetryBackoffStrategy
+    };
+} // skipcq: RS-D1001
