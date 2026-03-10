7c230459aac12c83524cf091e295dc5df5b0f891
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-03-10 10:38:38 +0200

    Improved upon the macro and renamed it to every!(...) for better semantics

--- proc-core/src/lib.rs
+++ proc-core/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
-use std::ops::RangeInclusive;
+use std::ops::{Range, RangeInclusive};
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input, LitInt, Token};
+use syn::{DeriveInput, parse_macro_input, Token, Expr, ExprLit, Lit};
 use strsim::levenshtein;
 use syn::parse::{Parse, ParseStream};
 
@@ -22,14 +22,44 @@ pub fn cron(input: TokenStream) -> TokenStream {
 }
 
 struct Interval {
-    days: u16,
-    hours: u16,
-    minutes: u16,
-    seconds: u16,
-    millis: u16
+    days: f64,
+    hours: f64,
+    minutes: f64,
+    seconds: f64,
+    millis: f64
 }
 
-const RANGES: [RangeInclusive<u16>; 5] = [0..=999, 0..=59, 0..=59, 0..=59, 0..=31];
+enum RangeType {
+    Bounded(Range<f64>),
+    Inclusive(RangeInclusive<f64>),
+}
+
+impl RangeType {
+    fn contains(&self, num: &f64) -> bool {
+        match self {
+            RangeType::Bounded(range) => range.contains(&num) && *num != 0.0,
+            RangeType::Inclusive(range) => range.contains(&num) && *num != 0.0,
+        }
+    }
+}
+
+impl ToString for RangeType {
+    fn to_string(&self) -> String {
+        match self {
+            RangeType::Bounded(range) => format!("{}..{}", range.start, range.end),
+            RangeType::Inclusive(range) => format!("{}..={}", range.start(), range.end()),
+        }
+    }
+}
+
+const RANGES: [RangeType; 5] = [
+    RangeType::Bounded(0.0..1000.0),
+    RangeType::Bounded(0.0..60.0),
+    RangeType::Bounded(0.0..60.0),
+    RangeType::Bounded(0.0..60.0),
+    RangeType::Inclusive(0.0..=31.0)
+];
+
 const TIME_FIELD: [&str; 5] = ["milliseconds", "seconds", "minutes", "hours", "days"];
 const SUFFIXES: [&str; 5] = ["ms", "s", "m", "h", "d"];
 
@@ -43,15 +73,15 @@ fn extract_expected_values(ptr: usize) -> String {
     }
 }
 
-fn search_suffixes<'a>(lit: &LitInt) -> Result<(&'a RangeInclusive<u16>, usize), (usize, &'a str)> {
+fn search_suffixes<'a>(target: &str) -> Result<(&'a RangeType, usize), (usize, &'a str)> {
     let mut min_pair = (usize::MAX, "");
     for (idx, suffix) in SUFFIXES.iter().enumerate() {
-        if *suffix == lit.suffix() {
+        if *suffix == target {
             let range = &RANGES[idx];
             return Ok((range, idx));
         }
 
-        let dist = levenshtein(&lit.suffix(), suffix);
+        let dist = levenshtein(&target, suffix);
         if min_pair.0 > dist {
             min_pair = (dist, *suffix);
         }
@@ -94,12 +124,34 @@ fn handle_seperator_format(
     }
 }
 
+macro_rules! parse_as_positive_fraction {
+    ($lit: expr, $lit_span: expr, $name: expr) => {{
+        let num = $lit.base10_parse::<f64>().map_err(|_| {
+            syn::Error::new(
+                $lit_span,
+                format!("Expected a {} but got \"{}\"", $name, $lit)
+            )
+        })?;
+
+        if num <= 0f64 {
+            return Err(syn::Error::new(
+                $lit_span,
+                format!("Expected a {} but got \"{}\"", $name, $lit)
+            ))
+        }
+
+        num
+    }};
+}
+
 impl Parse for Interval {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut ptr = SUFFIXES.len();
-        let mut parts: [u16; 5] = [0, 0, 0, 0, 0];
+        let mut parts: [f64; 5] = [0.0, 0.0, 0.0, 0.0, 0.0];
         let seperator_format = input.peek2(Token![,]);
         let mut expecting_seperator = false;
+        let mut encountered_fractional = false;
+        let mut has_modified = false;
 
         while !input.is_empty() {
             let is_seperator = input.cursor()
@@ -110,32 +162,61 @@ impl Parse for Interval {
                 continue;
             }
 
+            let lit_span = input.cursor().span();
+
             expecting_seperator = !expecting_seperator;
 
-            let lit_span = input.cursor().span();
-            let Ok(lit) = input.parse::<LitInt>() else {
+            let Ok(lit) = input.parse::<Expr>() else {
                 return Err(syn::Error::new(
                     lit_span,
-                    "Expected an unsigned integer but got something else"
+                    "Expected a positive integer or float literal but got something else"
                 ));
             };
 
-            let Ok(num) = lit.base10_parse::<u16>() else {
-                return Err(syn::Error::new(
-                    lit_span,
-                    format!("Expected an unsigned integer (u16) but got \"{lit}\"")
-                ))
+            let (num, suffix) = match lit {
+                Expr::Lit(ExprLit { lit: Lit::Int(lit), .. }) => {
+                    if encountered_fractional {
+                        return Err(syn::Error::new(
+                            lit_span,
+                            "Unexpected integer followed after fractional part"
+                        ));
+                    }
+
+                    let num = parse_as_positive_fraction!(lit, lit_span, "positive integer");
+
+                    (num, lit.suffix().to_string())
+                }
+
+                Expr::Lit(ExprLit { lit: Lit::Float(lit), .. }) => {
+                    if encountered_fractional {
+                        return Err(syn::Error::new(
+                            lit_span,
+                            "Fractional parts are allowed only at the lowest time field"
+                        ));
+                    }
+
+                    let num = parse_as_positive_fraction!(lit, lit_span, "positive float");
+                    encountered_fractional = true;
+
+                    (num, lit.suffix().to_string())
+                }
+
+                _ => {
+                    return Err(syn::Error::new(
+                        lit_span,
+                        "Expected a positive integer or float literal but got something else"
+                    ));
+                }
             };
 
-            match search_suffixes(&lit) {
+            match search_suffixes(&suffix) {
                 Ok((range, pos)) => {
                     if !range.contains(&num) {
                         return Err(syn::Error::new(
                             lit_span,
                             format!(
-                                "Exceeded expected range of {}..={} for \"{}\" time field, got \"{num}\"",
-                                range.start(),
-                                range.end(),
+                                "Exceeded expected range of {} for \"{}\" time field, got \"{num}\"",
+                                range.to_string(),
                                 TIME_FIELD[pos]
                             )
                         ))
@@ -160,17 +241,18 @@ impl Parse for Interval {
                     ptr = pos;
 
                     parts[pos] = num;
+                    has_modified = true;
 
                     continue;
                 },
 
                 Err((dist, expected)) => {
-                    let msg = if dist < 2 {
-                        format!("Unexpected suffix \"{}\", did you mean \"{}\"", lit.suffix(), expected)
-                    } else if lit.suffix() == "" {
+                    let msg = if suffix == "" {
                         "Missing time unit suffix (expected one of: ms, s, m, h, d)".to_string()
+                    } else if dist < 2 {
+                        format!("Unexpected suffix \"{}\", did you mean \"{}\"", suffix, expected)
                     } else {
-                        format!("Unexpected suffix \"{}\"", lit.suffix())
+                        format!("Unexpected suffix \"{}\"", suffix)
                     };
 
                     return Err(syn::Error::new(
@@ -179,6 +261,13 @@ impl Parse for Interval {
                     ));
                 }
             }
+        }
+
+        if !has_modified {
+            return Err(syn::Error::new(
+                input.span(),
+                "Expected time field literals got nothing"
+            ));
         }
 
         Ok(Self {
@@ -192,13 +281,13 @@ impl Parse for Interval {
 }
 
 #[proc_macro]
-pub fn interval(input: TokenStream) -> TokenStream {
+pub fn every(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as Interval);
-    let sum = (input.millis as f64 / 1000.0)
-        + (input.seconds as f64)
-        + (input.minutes as f64 * 60.0)
-        + (input.hours as f64 * 3600.0)
-        + (input.days as f64 * 86400.0);
+    let sum = (input.millis / 1000.0)
+        + (input.seconds)
+        + (input.minutes * 60.0)
+        + (input.hours * 3600.0)
+        + (input.days * 86400.0);
 
     TokenStream::from(quote! { chronographer::task::interval::TaskScheduleInterval::from_secs_f64(#sum) })
 }
