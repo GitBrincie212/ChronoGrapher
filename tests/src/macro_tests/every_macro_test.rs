50b8dd9455481e5b1d3366e3cdb0b9fa8736e053
m-epasta <mepastaacc@outlook.com>
2026-03-16 12:23:05 +0100

    Refactored and added more tests into every_macro_test

--- tests/src/macro_tests/every_macro_test.rs
+++ tests/src/macro_tests/every_macro_test.rs
@@ -14,60 +14,94 @@ macro_rules! assert_every {
     ($expected:expr, $($args:tt)*) => {
         let interval = every!($($args)*);
         let duration = simulate_duration(interval);
-        assert_eq!(duration, $expected);
+        assert_eq!(duration.as_secs_f64(), $expected as f64);
     };
 }
 
-const MINUTE: Duration = Duration::from_secs(60);
-const HOUR: Duration = Duration::from_secs(3600);
-const DAY: Duration = Duration::from_secs(86400);
+const MINUTE: f64 = 60.0;
+const HOUR: f64 = 3600.0;
+const DAY: f64 = 86400.0;
 
 #[test]
 fn test_seconds() {
-    assert_every!(Duration::from_secs(2), 2s);
-    assert_every!(Duration::from_secs_f64(2.5), 2.5s);
+    assert_every!(2.0, 2s);
+    assert_every!(2.5, 2.5s);
+    assert_every!(59.999, 59.999s);
 }
 
 #[test]
 fn test_millis() {
-    assert_every!(Duration::from_millis(1), 1ms);
+    assert_every!(0.001, 1ms);
+    assert_every!(0.000001, 0.001ms);
+    assert_every!(0.999999, 999.999ms);
 }
 
 #[test]
 fn test_minutes() {
-    assert_every!(MINUTE * 3, 3m);
+    assert_every!(MINUTE * 3.0, 3m);
+    assert_every!(MINUTE * 59.999, 59.999m);
 }
 
 #[test]
 fn test_hours() {
-    assert_every!(HOUR * 4, 4h);
+    assert_every!(HOUR * 4.0, 4h);
+    assert_every!(HOUR * 59.999, 59.999h);
 }
 
 #[test]
 fn test_days() {
-    assert_every!(DAY * 5, 5d);
-    assert_every!(Duration::from_secs_f64(5.12 * 86400.0), 5.12d);
+    assert_every!(DAY * 5.0, 5d);
+    assert_every!(5.12 * 86400.0, 5.12d);
+    assert_every!(DAY * 31.0, 31d);
 }
 
 #[test]
 fn test_sep() {
+    assert_every!(MINUTE * 3.0 + 2.0 + 0.001, 3m, 2s, 1ms);
+    assert_every!(DAY * 5.0 + 2.0, 5d, 2s);
     assert_every!(
-        MINUTE * 3 + Duration::from_secs(2) + Duration::from_millis(1),
-        3m,
-        2s,
+        DAY * 1.0 + HOUR * 1.0 + MINUTE * 1.0 + 1.0 + 0.001,
+        1d,
+        1h,
+        1m,
+        1s,
         1ms
     );
-    assert_every!(DAY * 5 + Duration::from_secs(2), 5d, 2s);
+    assert_every!(
+        DAY * 31.0 + HOUR * 59.0 + MINUTE * 59.0 + 59.0 + 0.9999,
+        31d,
+        59h,
+        59m,
+        59s,
+        999.9ms
+    );
+    assert_every!(DAY * 1.0 + 0.001, 1d, 1ms);
+    assert_every!(HOUR * 1.0 + 1.0, 1h, 1s);
 }
 
 #[test]
 fn test_spaced() {
-    assert_every!(MINUTE * 3 + Duration::from_secs(2) + Duration::from_millis(1), 3m 2s 1ms);
-    assert_every!(DAY * 5 + Duration::from_secs(2), 5d 2s);
+    assert_every!(MINUTE * 3.0 + 2.0 + 0.001, 3m 2s 1ms);
+    assert_every!(DAY * 5.0 + 2.0, 5d 2s);
+    assert_every!(DAY * 1.0 + HOUR * 1.0 + MINUTE * 1.0 + 1.0 + 0.001, 1d 1h 1m 1s 1ms);
 }
 
 #[test]
-fn test_macro_errors() {
+fn test_scientific_notation() {
+    assert_every!(10.0, 1e1s);
+    assert_every!(12.0, 1.2e1s);
+    assert_every!(0.1, 1e-1s);
+}
+
+#[test]
+fn test_mixed_floats() {
+    assert_every!(DAY * 1.0 + HOUR * 1.0 + MINUTE * 1.5, 1d, 1h, 1.5m);
+    assert_every!(DAY * 1.0 + HOUR * 2.5, 1d, 2.5h);
+    assert_every!(DAY * 30.5, 30.5d);
+}
+
+#[test]
+fn test_macro_structure() {
     let t = trybuild::TestCases::new();
     t.compile_fail("ui/every_errors.rs");
 }
