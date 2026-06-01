525559523679d9466f1204fd28f47192633e32c4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-06-01 14:16:55 +0300

    Small fix

--- bin/src/test3.rs
+++ bin/src/test3.rs
@@ -1,13 +1,5 @@
 use chronographer::prelude::*;
 
-#[taskframe(__internal_workflow_spec = (
-    retry(3),
-    timeout(5s)
-))]
-pub async fn MyTaskFrame(ctx: &TaskFrameContext) -> Result<(), String> {
-    todo!()
-}
-
 #[chronographer::main]
 pub async fn main(scheduler: DefaultLiveScheduler<String>) {
     todo!()
