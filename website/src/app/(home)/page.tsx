c748d8db4c5e6f120d9a746f12ff50e926436bb7
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-06-17 17:36:40 +0300

    Removed the integrations section and planning to remake the flow

--- website/src/app/(home)/page.tsx
+++ website/src/app/(home)/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import HeroSection from "@/components/home/sections/HeroSection";
-import IntegrationsSection from "@/components/home/sections/IntegrationsSection";
 
 export default function HomePage() {
   return (
     <main className="overflow-x-hidden">
       <HeroSection />
-      <IntegrationsSection />
     </main>
   );
 }
