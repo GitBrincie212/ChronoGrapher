c7ae97ecb1f49ae900bad009430c610c447d698a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-07-15 23:55:11 +0300

    Altered the timeout variant to allow for explicit typing for dynamic or constant expressions

--- proc/src/workflow/timeout.rs
+++ proc/src/workflow/timeout.rs
@@ -1,12 +1,64 @@
 use crate::utils::TimeLiteral;
 use crate::workflow::utils::{ArgumentParser, ValueSource, WorkflowTransform};
 use proc_macro2::TokenStream as TokenStream2;
-use quote::quote;
-use syn::parse::{Parse, ParseStream};
+use quote::{quote, ToTokens};
+use syn::parenthesized;
+use syn::parse::{Parse, ParseBuffer, ParseStream};
+
+enum OnTimeoutVariants {
+    Constant(syn::Expr),
+    Dynamic(syn::Expr)
+}
+
+fn try_dynamic_path(fork: ParseBuffer) -> syn::Result<OnTimeoutVariants> {
+    let ident = fork.parse::<syn::Ident>()?;
+    let content;
+    parenthesized!(content in fork);
+
+    if ident.to_string() != "dynamic" {
+        return Err(syn::Error::new_spanned(ident, "Expected 'dynamic' but got something else"));
+    }
+
+    let expr = content.parse::<syn::Expr>()?;
+    match expr {
+        syn::Expr::Closure(_) => Ok(OnTimeoutVariants::Dynamic(expr)),
+        syn::Expr::Path(_) => Ok(OnTimeoutVariants::Dynamic(expr)),
+        syn::Expr::Macro(_) => Ok(OnTimeoutVariants::Dynamic(expr)),
+        _ => Err(syn::Error::new_spanned(expr, "Expected a macro, closure or function identifier"))
+    }
+}
+
+impl Parse for OnTimeoutVariants {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if let Ok(value) = try_dynamic_path(input.fork()) {
+            return Ok(value);
+        }
+
+        let expr = input.parse()?;
+        match expr {
+            syn::Expr::Path(_) => Ok(OnTimeoutVariants::Constant(expr)),
+            syn::Expr::Macro(_) => Ok(OnTimeoutVariants::Constant(expr)),
+            _ => Err(syn::Error::new_spanned(expr, "Expected an error identifier or macro"))
+        }
+    }
+}
+
+impl ToTokens for OnTimeoutVariants {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        match self {
+            OnTimeoutVariants::Constant(expr) => {
+                let expanded = quote! { || #expr };
+                expanded.to_tokens(tokens);
+            },
+
+            OnTimeoutVariants::Dynamic(expr) => expr.to_tokens(tokens),
+        }
+    }
+}
 
 pub struct TimeoutArguments {
     duration: ValueSource<TimeLiteral>,
-    on_timeout: Option<syn::Expr>,
+    on_timeout: Option<OnTimeoutVariants>,
 }
 
 impl Parse for TimeoutArguments {
