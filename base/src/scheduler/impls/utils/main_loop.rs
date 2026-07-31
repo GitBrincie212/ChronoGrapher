8e1b7809212eb9d37db19c44b86c2a95541bc096
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-07-31 19:57:08 +0300

    Slightly optimized and cleaned up the code for LiveScheduler

--- base/src/scheduler/impls/utils/main_loop.rs
+++ base/src/scheduler/impls/utils/main_loop.rs
@@ -1,23 +1,23 @@
 use crate::scheduler::engine::SchedulerEngine;
 use crate::scheduler::impls::live::SchedulerWorkerHot;
 use crate::scheduler::impls::utils::spawn_task;
-use crate::scheduler::{SchedulerConfig, SchedulerWorkerCold};
+use crate::scheduler::{LiveSchedulerComponents, SchedulerConfig, SchedulerWorkerCold};
 use crossbeam::utils::CachePadded;
 use std::sync::Arc;
 
 #[inline(always)]
 pub fn main_loop_logic<C: SchedulerConfig>(
-    engine: &Arc<C::SchedulerEngine>,
+    components: &Arc<LiveSchedulerComponents<C>>,
     hot_workers: &Arc<Vec<CachePadded<SchedulerWorkerHot<C>>>>,
     cold_workers: &Arc<Vec<CachePadded<SchedulerWorkerCold<C>>>>,
 ) -> impl Future<Output = ()> + 'static {
-    let engine = engine.clone();
+    let components = components.clone();
     let hot_workers = hot_workers.clone();
     let cold_workers = cold_workers.clone();
 
     async move {
         loop {
-            for id in engine.retrieve().await {
+            for id in components.engine.retrieve().await {
                 spawn_task::<C>(id, &hot_workers, &cold_workers);
             }
         }
