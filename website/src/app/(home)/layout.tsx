4a9d996c6ebfdf7c649851e3ff9a7d43e03dcd1c
Samy <144945394+ByHolySamurai@users.noreply.github.com>
2026-08-01 19:30:07 +0700

    Implement language toggle in navigation

--- website/src/app/(home)/layout.tsx
+++ website/src/app/(home)/layout.tsx
@@ -1,6 +1,20 @@
 import { HomeLayout } from "fumadocs-ui/layouts/home";
+import ProgrammingLangToggle from "@/components/ui/toggle-language";
 import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/">) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions()}
+      links={[
+        {
+          type: "custom",
+          secondary: true,
+          children: <ProgrammingLangToggle variant="nav" />,
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
