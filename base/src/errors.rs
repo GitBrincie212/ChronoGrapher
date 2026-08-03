f2852e2d7c26bb0f9a43b9dee6f5aca223ce1135
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-08-03 14:51:25 +0300

    Removed all unused errors that were left due to the old model

--- base/src/errors.rs
+++ base/src/errors.rs
@@ -1,6 +1,5 @@
 use std::any::Any;
 use std::fmt::{Debug, Display};
-use std::time::Duration;
 use thiserror::Error;
 
 pub trait TaskError: Debug + Display + Send + Sync + 'static {
@@ -13,59 +12,6 @@ impl<T: Debug + Display + Send + Sync + Any> TaskError for T {
     }
 }
 
-#[derive(Error, Debug)]
-pub enum ConditionalTaskFrameError<T1: TaskError, T2: TaskError> {
-    #[error(
-        "ConditionalTaskFrame has failed, with the error originating from primary TaskFrame's failure:\n\t{0}"
-    )]
-    PrimaryFailed(T1),
-
-    #[error(
-        "ConditionalTaskFrame has failed, with the error originating from secondary TaskFrame's failure:\n\t{0}"
-    )]
-    SecondaryFailed(T2),
-
-    #[error("ConditionalTaskFrame has returned false with `error_on_false` enabled")]
-    TaskConditionFail,
-}
-
-#[derive(Error, Debug)]
-pub enum TimeoutTaskFrameError<T: TaskError> {
-    #[error(
-        "TimeoutTaskFrame has failed, with the error originating from primary TaskFrame's failure:\n\t{0}"
-    )]
-    Inner(T),
-
-    #[error("TimeoutTaskFrame has timeout with max duration '{0:?}'")]
-    Timeout(Duration),
-}
-
-#[derive(Error, Debug)]
-pub enum ThresholdTaskFrameError<T: TaskError> {
-    #[error(
-        "ThresholdTaskFrame has failed, with the error originating from primary TaskFrame's failure:\n\t{0}"
-    )]
-    Inner(T),
-
-    #[error(
-        "ThresholdTaskFrame has failed with the error originating from the \"DependentFailBehavior\""
-    )]
-    ThresholdReachedWithFailure,
-}
-
-#[derive(Error, Debug)]
-pub enum DependencyTaskFrameError<T: TaskError> {
-    #[error(
-        "DependencyTaskFrame has failed, with the error originating from inner TaskFrame's failure:\n\t{0}"
-    )]
-    Inner(T),
-
-    #[error(
-        "DependencyTaskFrame has failed with the error originating from the \"DependentFailBehavior\":\n\t'{0}'"
-    )]
-    DependenciesInvalidated(Box<dyn TaskError>),
-}
-
 #[derive(Error, Debug, PartialEq, Eq)]
 #[error(
     "Task frame index `{index}` is out of bounds for `{src}` with task frame size `{size}` element(s)"
@@ -75,10 +21,6 @@ pub struct TaskSelectionIndexOutOfBounds {
     pub src: String,
     pub size: usize,
 }
-
-#[derive(Error, Debug, PartialEq, Eq)]
-#[error("Dependencies have not been resolved (errored due to the use of 'DependentFailBehavior')")]
-pub struct TaskDependenciesUnresolved;
 
 #[cfg(feature = "chrono")]
 #[derive(Error, Debug, PartialEq, Eq)]
