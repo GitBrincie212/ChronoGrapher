f8895cb2b44a17c7d3acd93de0a324e457870fa1
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-08-07 17:09:23 +0300

    Added a helper internal macro for generating payload wrapper types more ergonomically

--- base/src/utils.rs
+++ base/src/utils.rs
@@ -52,6 +52,49 @@ pub(crate) mod macros {
         };
     }
 
+    macro_rules! payload_wrapper {
+        ($(#[$($attrss:tt)*])* $name: ident ($($toks: tt)*)) => {
+            $(#[$($attrss)*])*
+            #[repr(transparent)]
+            pub struct $name($($toks)+);
+
+            impl From<$name> for $($toks)+ {
+                fn from(value: $name) -> Self {
+                    value.0
+                }
+            }
+
+            impl Deref for $name {
+                type Target = $($toks)+;
+
+                fn deref(&self) -> &Self::Target {
+                    &self.0
+                }
+            }
+        };
+
+        ($(#[$($attrss:tt)*])* $name: ident <$($lt: lifetime),*> ($($toks: tt)*)) => {
+            $(#[$($attrss)*])*
+            #[repr(transparent)]
+            pub struct $name<$($lt),*>($($toks)+);
+
+            impl<$($lt),*> From<$name<$($lt),*>> for $($toks)+ {
+                fn from(value: $name<$($lt),*>) -> Self {
+                    value.0
+                }
+            }
+
+            impl<$($lt),*> Deref for $name<$($lt),*> {
+                type Target = $($toks)+;
+
+                fn deref(&self) -> &Self::Target {
+                    &self.0
+                }
+            }
+        };
+    }
+
     pub(crate) use define_event;
     pub(crate) use define_event_group;
+    pub(crate) use payload_wrapper;
 }
