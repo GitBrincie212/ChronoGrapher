5d61b818b17bdf040f6ef225f0cb881672f0dd7f
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-08-08 14:45:34 +0300

    add `LinearBackoffStrategy` bench functions

--- benches/src/backoff.rs
+++ benches/src/backoff.rs
@@ -14,3 +14,25 @@ fn constant(bencher: divan::Bencher, retry: u32) {
     });
 }
 
+#[divan::bench(args = RETRIES)]
+fn linear(bencher: divan::Bencher, retry: u32) {
+    let strategy = LinearBackoffStrategy::builder()
+        .factor(Duration::from_millis(100))
+        .start(Duration::from_millis(10))
+        .build();
+    bencher.bench(|| {
+        divan::black_box(strategy.compute(divan::black_box(retry)));
+    });
+}
+
+#[divan::bench(args = RETRIES)]
+fn linear_bounded(bencher: divan::Bencher, retry: u32) {
+    let strategy = LinearBackoffStrategy::builder()
+        .factor(Duration::from_millis(100))
+        .clamp(Duration::from_secs(30))
+        .build();
+    bencher.bench(|| {
+        divan::black_box(strategy.compute(divan::black_box(retry)));
+    });
+}
+
