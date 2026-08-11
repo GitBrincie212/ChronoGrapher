f2d9275d9ad4d79ae37af360df6b02f61b0f6d72
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-08-11 13:12:12 +0300

    Fixed some clippy issues for DependencyTaskFrame

--- base/src/task/frames/dependencyframe.rs
+++ base/src/task/frames/dependencyframe.rs
@@ -515,6 +515,6 @@ impl<T: TaskFrame> TaskFrame for DependencyTaskFrame<T> {
             return self.unresolve.execute();
         }
 
-        self.frame.execute(&ctx, args).await
+        self.frame.execute(ctx, args).await
     }
 }
