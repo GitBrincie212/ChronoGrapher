83e02d4d5385d7dd58fcb54dfdc48ee558a27a6a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-08-12 12:29:13 +0300

    Fixed clippy issues

--- proc/src/task.rs
+++ proc/src/task.rs
@@ -121,12 +121,11 @@ pub fn task(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let workflow_type = workflow_toks
         .clone()
-        .map(|x| {
+        .and_then(|x| {
             Punctuated::<WorkflowPrimitive, Token![,]>::parse_terminated
                 .parse2(x)
                 .ok()
         })
-        .flatten()
         .unwrap_or_else(Punctuated::new)
         .iter()
         .fold(quote! { #taskframe_name #temp }, |acc, x| x.get_type(acc));
